a8ba47efc17471ac5eda0e53f9f26ba6b5452991
Vincent Velociter <vincent.velociter@gmail.com>
2014-05-09 20:19:08 +0200

    Gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
+var jshint = require('gulp-jshint');
 
-// Basic usage
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('src/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// browserify
 gulp.task('scripts', function() {
     // Single entry point to browserify
     gulp.src('src/app.js')
@@ -11,3 +19,11 @@ gulp.task('scripts', function() {
         }))
         .pipe(gulp.dest('www/'));
 });
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('src/*.js', ['lint', 'scripts']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'scripts', 'watch']);

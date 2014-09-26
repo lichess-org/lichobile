d3e0f656650c3d5f20c5a68242ab05cd75d8915d
Jacques <jba@zengularity.com>
2014-09-26 14:07:13 +0200

    More html with a bit of css

--- src/js/view.js
+++ src/js/view.js
@@ -3,8 +3,33 @@
 var chessground = require('chessground');
 var m = require('mithril');
 
+function renderHeader(){
+  return m('header', [
+    m('span.lichess', ["%"]),
+    m('h1.title', [ "lichess.org" ]),
+    m('span.lichess', ["r"]),
+  ]);
+}
+
+function renderBoard(ctrl){
+  return m('div.content', [
+    m('div.chessground.wood.merida', [
+      chessground.view(ctrl.chessground)
+    ])
+  ]);
+}
+
+function renderFooter(){
+  return m('footer', [
+    m('span.lichess', ["h"]),
+    m('span.lichess', ["G"])
+  ]);
+}
+
 module.exports = function(ctrl) {
-  return m('div.chessground.wood.merida', [
-    chessground.view(ctrl.chessground)
+  return m('div', [
+    renderHeader(),
+    renderBoard(ctrl),
+    renderFooter()
   ]);
 };

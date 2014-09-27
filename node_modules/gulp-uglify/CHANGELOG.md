bc2ba0f7dd36407089dee8a4eaf7ae5a30993aa7
Vincent Velociter <vincent.velociter@gmail.com>
2014-09-27 13:47:06 +0200

    Update devDependencies

--- node_modules/gulp-uglify/CHANGELOG.md
+++ node_modules/gulp-uglify/CHANGELOG.md
@@ -1,5 +1,14 @@
 # gulp-uglify changelog
 
+## 1.0.0
+
+- Handle cases where UglifyJS uses e.msg instead of e.message for error codes. Fixes #51.
+- Supplement UglifyJS’s source map merging with vinyl-sourcemap-apply to correct issues where `sources` and `sourcesContent` were different. Fixes #43.
+- Refactor option parsing and defaults, and calls to uglify-js, to reduce complexity of the main function.
+- Added tests for the previously forgotten `preserveComments` option.
+- Updated UglifyJS to 2.4.15.
+- Changed dependencies to explicit ranges to avoid `node-semver` issues.
+
 ## 0.3.2
 
 - Removed the PluginError factory wrapper
--- node_modules/gulp-uglify/test/err.js
+++ node_modules/gulp-uglify/test/err.js
@@ -4,12 +4,20 @@ var test = require('tape'),
 		gulpUglify = require('../');
 	
 var testContentsInput = 'function errorFunction(error)\n{';
+var testOkContentsInput = '"use strict"; (function(console, first, second) { console.log(first + second) }(5, 10))';
 
 var testFile1 = new Vinyl({
 	cwd: "/home/terin/broken-promises/",
 	base: "/home/terin/broken-promises/test",
 	path: "/home/terin/broken-promises/test/test1.js",
 	contents: new Buffer(testContentsInput)
+});
+
+var testFile2 = new Vinyl({
+	cwd: "/home/terin/broken-promises/",
+	base: "/home/terin/broken-promises/test",
+	path: "/home/terin/broken-promises/test/test2.js",
+	contents: new Buffer(testOkContentsInput)
 });
 
 test('should report files in error', function(t) {
@@ -31,5 +39,30 @@ test('should report files in error', function(t) {
 	});
 
 	stream.write(testFile1);
+	stream.end();
+});
+
+test('shouldn\'t blow up when given output options', function(t) {
+	t.plan(5);
+
+	var stream = gulpUglify({
+		output: {
+			exportAll: true
+		}
+	});
+
+	stream.on('data', function() {
+		t.fail('We shouldn\'t have gotten here');
+	});
+
+	stream.on('error', function(e) {
+		t.ok(e instanceof Error, 'argument should be of type error');
+		t.equals(e.message, '`exportAll` is not a supported option');
+		t.equal(e.plugin, 'gulp-uglify', 'error is from gulp-uglify');
+		t.equal(e.fileName, testFile2.path, 'error reports correct file name');
+		t.false(e.showStack, 'error is configured to not print the stack');
+	});
+
+	stream.write(testFile2);
 	stream.end();
 });

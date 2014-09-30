58d271856cd52664e893417d26264bdf01586cc6
Vincent Velociter <vincent.velociter@gmail.com>
2014-09-30 20:11:53 +0200

    Update chessground again

--- node_modules/chessground/src/api.js
+++ node_modules/chessground/src/api.js
@@ -2,26 +2,18 @@ var m = require('mithril');
 
 module.exports = function(element, controller, view) {
 
-  var action = function(computation) {
-    return function() {
-      m.startComputation();
-      var result = computation.apply(null, arguments);
-      m.endComputation();
-      return result;
-    };
-  };
-
   return {
-    set: action(controller.reconfigure),
-    toggleOrientation: action(controller.toggleOrientation),
+    set: controller.reconfigure,
+    toggleOrientation: controller.toggleOrientation,
     getOrientation: controller.getOrientation,
     getPieces: controller.getPieces,
     getFen: controller.getFen,
     dump: function() {
       return controller.data;
     },
-    move: action(controller.apiMove),
-    setPieces: action(controller.setPieces),
-    playPremove: action(controller.playPremove)
+    move: controller.apiMove,
+    setPieces: controller.setPieces,
+    setCheck: controller.setCheck,
+    playPremove: controller.playPremove
   };
 };

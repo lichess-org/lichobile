31dab5cb1709973a817aeffb6b23c451a9497bc3
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-09-30 20:29:17 +0200

    get rid of node_modules in the git repo

--- node_modules/cordova/node_modules/cordova-lib/templates/base.js
+++
@@ -1,5 +0,0 @@
-var exec = require('cordova/exec');
-
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "%pluginName%", "coolMethod", [arg0]);
-};
--- node_modules/iscroll/src/zoom/refresh.js
+++
@@ -1,6 +0,0 @@
-
-	this.scrollerWidth	= Math.round(this.scroller.offsetWidth * this.scale);
-	this.scrollerHeight	= Math.round(this.scroller.offsetHeight * this.scale);
-
-	this.maxScrollX		= this.wrapperWidth - this.scrollerWidth;
-	this.maxScrollY		= this.wrapperHeight - this.scrollerHeight;

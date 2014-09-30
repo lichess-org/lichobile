96914d867243a7439a63b232054663676cb1e5f9
Vincent Velociter <vincent.velociter@gmail.com>
2014-09-30 21:01:32 +0200

    Simplify Game

--- src/js/game/game.js
+++ src/js/game/game.js
@@ -2,36 +2,32 @@
 
 var _ = require('lodash');
 
-// Object that holds game data and receives updates from server
-module.exports = function(data) {
-  var game = {};
+// Object that holds game lichessData and receives updates from server
+module.exports = function(lichessData) {
+  var data = {};
   var player = {};
   var opponent = {};
   var possibleMoves = {};
   var url = {};
   var clock = null;
 
-  if (_.isObject(data)) {
-    game = data.game;
-    player = data.player;
-    opponent = data.opponent;
-    possibleMoves = data.possibleMoves;
-    clock = data.clock;
-    url = data.url;
+  if (_.isObject(lichessData)) {
+    data = lichessData.game;
+    player = lichessData.player;
+    opponent = lichessData.opponent;
+    possibleMoves = lichessData.possibleMoves;
+    clock = lichessData.clock;
+    url = lichessData.url;
   }
 
   function fullId() {
-    if (player.id) return game.id + player.id;
-    return game.id;
+    if (player.id) return data.id + player.id;
+    return data.id;
   }
 
   function updateState(state) {
-    game.player = state.color;
-    game.turns = state.turns;
-  }
-
-  function getFen() {
-    return game.fen;
+    data.player = state.color;
+    data.turns = state.turns;
   }
 
   function getPossibleMoves() {
@@ -45,51 +41,35 @@ module.exports = function(data) {
   }
 
   function isMyTurn() {
-    return game.player === player.color;
+    return data.player === player.color;
   }
 
   function isMoveAllowed(from, to) {
     var m = possibleMoves[from];
-    return m && _.indexOf(m.match(/.{1,2}/g), to) !== -1;
+    return m && _.indexOf(m.match(/.{2}/g), to) !== -1;
   }
 
   function lastPlayer() {
-    return (game.player === 'white') ? 'black' : 'white';
+    return (data.player === 'white') ? 'black' : 'white';
   }
 
   function currentPlayer() {
-    return game.player;
+    return data.player;
   }
 
   function currentTurn() {
-    return game.turns;
-  }
-
-  function startedAtTurn() {
-    return game.startedAtTurn;
+    return data.turns;
   }
 
   function lastMove() {
-    return game.lastMove ? {
-      from: game.lastMove.substr(0, 2),
-      to: game.lastMove.substr(2, 2)
+    return data.lastMove ? {
+      from: data.lastMove.substr(0, 2),
+      to: data.lastMove.substr(2, 2)
     } : null;
   }
 
   function hasClock() {
-    return game.clock;
-  }
-
-  function finish() {
-    game.finished = true;
-  }
-
-  function isFinished() {
-    return game.finished;
-  }
-
-  function isStarted() {
-    return game.started;
+    return data.clock;
   }
 
   return {
@@ -98,28 +78,23 @@ module.exports = function(data) {
     player: player,
     opponent: opponent,
     clock: clock,
-    speed: game.speed,
-    perf: game.perf,
-    variant: game.variant,
+    speed: data.speed,
+    perf: data.perf,
+    variant: data.variant,
 
     // public methods
     updateState: updateState,
-    finish: finish,
 
     fullId: fullId,
-    getFen: getFen,
     getPossibleMoves: getPossibleMoves,
     setPossibleMoves: setPossibleMoves,
     isMyTurn: isMyTurn,
     isMoveAllowed: isMoveAllowed,
-    startedAtTurn: startedAtTurn,
     currentTurn: currentTurn,
     currentPlayer: currentPlayer,
     lastPlayer: lastPlayer,
     lastMove: lastMove,
-    hasClock: hasClock,
-    isStarted: isStarted,
-    isFinished: isFinished
+    hasClock: hasClock
   };
 };
 

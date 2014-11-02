5a4cd9feae10f8d7a1e7e3eae6e3868cd3026a6b
Vincent Velociter <vincent.velociter@gmail.com>
2014-11-02 23:20:16 +0100

    Work on clocks

--- src/js/round/clock/view.js
+++ src/js/round/clock/view.js
@@ -1,3 +1,4 @@
+var m = require('mithril');
 var classSet = require('chessground').util.classSet;
 
 function prefixInteger(num, length) {
@@ -12,6 +13,7 @@ function formatClockTime(ctrl, time) {
   var date = new Date(time);
   var minutes = prefixInteger(date.getUTCMinutes(), 2);
   var seconds = prefixInteger(date.getSeconds(), 2);
+  var tenths;
   if (ctrl.data.showTenths && time < 10000) {
     tenths = Math.floor(date.getMilliseconds() / 100);
     return bold(minutes) + ':' + bold(seconds) + '<span>.' + bold(tenths) + '</span>';
@@ -23,22 +25,20 @@ function formatClockTime(ctrl, time) {
   }
 }
 
-module.exports = function(ctrl, color, position, runningColor) {
+module.exports = function(ctrl, color, runningColor) {
   var time = ctrl.data[color];
   return m('div', {
-    class: 'clock clock_' + color + ' clock_' + position + ' ' + classSet({
+    class: 'clock clock_' + color + ' ' + classSet({
       'outoftime': !time,
       'running': runningColor === color,
       'emerg': time < ctrl.data.emerg
     })
   }, [
-    ctrl.data.showBar ? m('div.bar',
-      m('span', {
-        style: {
-          width: Math.max(0, Math.min(100, (time / ctrl.data.barTime) * 100)) + 'px'
-        }
-      })
-    ) : null,
+    m('div.timer.before', {
+      style: {
+        width: Math.max(0, Math.min(100, (time / ctrl.data.initial) * 100)) + '%'
+      }
+    }),
     m('div.time', m.trust(formatClockTime(ctrl, time * 1000)))
   ]);
-}
+};
--- src/js/round/util.js
+++ src/js/round/util.js
@@ -4,4 +4,8 @@ util.str2move = function(m) {
   return m ? [m.slice(0, 2), m.slice(2, 4)] : null;
 };
 
+util.opponentColor = function(c) {
+  return c === 'white' ? 'black' : 'white';
+};
+
 module.exports = util;

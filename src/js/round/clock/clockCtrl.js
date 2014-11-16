2330cb03acc2ee7d84b6d399cc436cdf7220fceb
Vincent Velociter <vincent.velociter@gmail.com>
2014-11-16 19:09:54 +0100

    Cache dom access

--- src/js/round/clock/clockCtrl.js
+++ src/js/round/clock/clockCtrl.js
@@ -2,8 +2,14 @@ var formatClockTime = require('./clockView').formatClockTime;
 
 module.exports = function(data, outOfTime) {
   var lastUpdate;
-
-  this.data = data;
+  var lastTick = {
+    white: null,
+    black: null
+  };
+  var clockEls = {
+    white: null,
+    black: null
+  };
 
   function setLastUpdate() {
     lastUpdate = {
@@ -14,6 +20,14 @@ module.exports = function(data, outOfTime) {
   }
   setLastUpdate();
 
+  function hasDisplayedTimeChanged(color, time) {
+    var oldSeconds = new Date(lastTick[color]).getSeconds();
+    var newSeconds = new Date(time).getSeconds();
+    return (oldSeconds !== newSeconds) || time < 10000;
+  }
+
+  this.data = data;
+
   this.update = function(white, black) {
     this.data.white = white;
     this.data.black = black;
@@ -23,9 +37,20 @@ module.exports = function(data, outOfTime) {
   this.tick = function(color) {
     this.data[color] = Math.max(0, lastUpdate[color] - (new Date() - lastUpdate.at) / 1000);
     // performance hack: we don't want to call m.redraw() on every clock tick
-    document.getElementById('clock_' + color).innerHTML =
-    formatClockTime(this, this.data[color] * 1000);
+    var time = this.data[color] * 1000,
+    el;
+    if (clockEls[color])
+      el = clockEls[color];
+    else {
+      el = document.getElementById('clock_' + color);
+      clockEls[color] = el;
+    }
+    if (hasDisplayedTimeChanged(color, time)) {
+      el.innerHTML = formatClockTime(this, time);
+    }
+    lastTick[color] = time;
 
-    if (this.data[color] === 0) outOfTime();
+    if (this.data[color] === 0)
+      outOfTime();
   }.bind(this);
 };

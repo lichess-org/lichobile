f0779d281e07847cad9b0bbd64856a4bbfd5d823
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-01-06 17:13:31 +0100

    correspondence clock

---
+++ src/js/ui/round/correspondenceClock/correspondenceView.js
@@ -0,0 +1,40 @@
+var classSet = require('chessground').util.classSet;
+
+function prefixInteger(num, length) {
+  return (num / Math.pow(10, length)).toFixed(length).substr(2);
+}
+
+function formatClockTime(trans, time) {
+  var date = new Date(time);
+  var minutes = prefixInteger(date.getUTCMinutes(), 2);
+  var seconds = prefixInteger(date.getSeconds(), 2);
+  var hours, str = '';
+  if (time >= 86400 * 1000) {
+    // days : hours
+    var days = date.getUTCDate() - 1;
+    hours = date.getUTCHours();
+    str += (days === 1 ? trans('oneDay') : trans('nbDays', days)) + ' ';
+    if (hours !== 0) str += trans('nbHours', hours);
+  } else if (time >= 3600 * 1000) {
+    // hours : minutes
+    hours = date.getUTCHours();
+    str += prefixInteger(hours, 2) + ':' + minutes;
+  } else {
+    // minutes : seconds
+    str += minutes + ':' + seconds;
+  }
+  return str;
+}
+
+module.exports = function(ctrl, trans, color, runningColor) {
+  var time = ctrl.data[color];
+  return m('div', {
+    class: 'correspondence clock ' + classSet({
+      'outoftime': !time,
+      'running': runningColor === color,
+      'emerg': time < ctrl.data.emerg
+    })
+  }, [
+    m('div.time', formatClockTime(trans, time * 1000))
+  ]);
+}

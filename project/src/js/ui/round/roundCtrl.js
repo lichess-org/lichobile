4c34ffde44f0a7d40940a371a1a8afd17c8a5912
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-02-10 17:31:29 +0100

    remove unused roundCtrl.vm.redirecting

--- project/src/js/ui/round/roundCtrl.js
+++ project/src/js/ui/round/roundCtrl.js
@@ -25,7 +25,6 @@ module.exports = function(cfg) {
   this.vm = {
     connectedWS: true,
     flip: false,
-    redirecting: false,
     showingActions: false
   };
 
--- project/src/js/ui/round/view/roundView.js
+++ project/src/js/ui/round/view/roundView.js
@@ -115,12 +115,11 @@ function renderGameEndedActions(ctrl) {
   var winner = game.getPlayer(ctrl.data, ctrl.data.game.winner);
   var status = gameStatus.toLabel(ctrl.data) +
     (winner ? '. ' + i18n(winner.color === 'white' ? 'whiteIsVictorious' : 'blackIsVictorious') + '.' : '');
-  var buttons = ctrl.vm.redirecting ? null : (
+  var buttons =
     button.joinRematch(ctrl) ||
     button.answerOpponentRematch(ctrl) ||
     button.cancelRematch(ctrl) ||
-    [button.rematch(ctrl), m('br'), m('br'), button.backToGame(ctrl)]
-  );
+    [button.rematch(ctrl), m('br'), m('br'), button.backToGame(ctrl)];
   return [
     m('div.result', [result, m('br'), m('br'), status]),
     m('div.control.buttons', buttons)

91d356c08b13042fc58509d04c8ddd35800ada5c
Frank Calise <fcalise@gmail.com>
2015-03-24 09:04:56 -0400

    #20 implement friends api calls
    Next will need to add the scroller and update when data comes in (and
    sorting of names, if any)

--- project/src/js/ui/friends/index.js
+++ project/src/js/ui/friends/index.js
@@ -4,30 +4,72 @@ var widgets = require('../widget/common');
 var layout = require('../layout');
 var menu = require('../menu');
 var i18n = require('../../i18n');
+var xhr = require('../../xhr');
+var session = require('../../session');
+var socket = require('../../socket');
+
+var onlineFriends = [];
 
 function renderBody() {
-  dummyFriends = [
-    'thibault',
-  	'veloce',
-  	'settebello'
-  ];
-
   return [
-	m('ul.friends_list.general.scroller', {}, dummyFriends.map(function(f) {
-	  return m('li.list_item', {}, f);
+    m('ul.friends_list.general.scroller', {}, onlineFriends.map(function(f) {
+      return m('li.list_item', {}, f);
     }))
   ];
 }
 
 module.exports = {
   controller: function() {
-	helper.analyticsTrackView('Online Friends');
+	  
+    var friendsSocket;
+
+    helper.analyticsTrackView('Online Friends');
+
+    var toastName = function(name) {
+      window.plugins.toast.show(name, 'short', 'center');
+    }
+
+    var requestFriends = function() {
+      friendsSocket.send('following_onlines');
+    }
+
+    xhr.friends().then(function(data) {
+      friendsSocket = socket.connectFriends('v1', requestFriends, {
+        following_onlines: function(data) {
+          console.log('---------------------following onlines received');
+          console.log(JSON.stringify(data));
+          onlineFriends = data;
+          renderBody();
+          console.log('---------------------following onlines received');
+          /*data.forEach(function(f) {
+            console.log(f);
+          });*/
+        },
+        following_enters: function(name) {
+          console.log('follower came online ' + name);
+          toastName(name);
+        },
+        following_leaves: function(name) {
+          console.log('follower left: ' + name);
+          toastName(name);
+        }
+      });
+    });
+
+    this.onunload = function() {
+      if (friendsSocket) {
+        friendsSocket.destroy();
+        friendsSocket = null;
+      }
+    }.bind(this);
+
   },
 
   view: function() {
     var header = utils.partialf(widgets.header, null,
-	  widgets.backButton(i18n('onlineFriends'))
-	);
-	return layout.free(header, renderBody, widgets.empty, menu.view, widgets.empty);
+      widgets.backButton(i18n('onlineFriends'))
+    );
+
+    return layout.free(header, renderBody, widgets.empty, menu.view, widgets.empty);
   }
 };

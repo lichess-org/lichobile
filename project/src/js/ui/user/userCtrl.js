8054ad227d238e44398e66e15c6feecf8b22cad5
Frank Calise <fcalise@gmail.com>
2015-04-04 23:35:20 -0400

    WIP user profile

--- project/src/js/ui/user/userCtrl.js
+++ project/src/js/ui/user/userCtrl.js
@@ -1,18 +1,21 @@
 var utils = require('../../utils');
 var xhr = require('../../xhr');
 
+var userData = null;
+
 module.exports = function() {
   var userData;
 
   xhr.user(m.route.param('id')).then(function(data) {
     userData = data;
+    m.redraw();
   }, function(error) {
     utils.handleXhrError(error);
     m.route('/friends');
   });
 
   return {
-    getData: function() {
+    getUserData: function() {
       return userData;
     }
   };

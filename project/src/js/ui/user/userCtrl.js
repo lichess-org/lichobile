425b0d473c7253075e0b2971a720dcfaf3ceff33
Frank Calise <fcalise@gmail.com>
2015-04-08 18:35:49 -0400

    added user profile analytics tracking

--- project/src/js/ui/user/userCtrl.js
+++ project/src/js/ui/user/userCtrl.js
@@ -1,9 +1,13 @@
 var utils = require('../../utils');
 var xhr = require('../../xhr');
+var helper = require('../helper')
 
 var userData = null;
 
 module.exports = function() {
+
+  helper.analyticsTrackView('User Profile');
+
   var userData;
 
   xhr.user(m.route.param('id')).then(function(data) {

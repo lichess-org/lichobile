66553e5fe1e1f1e421e15eb7d397dca4dbcdf9a2
Vincent Velociter <vincent.velociter@gmail.com>
2015-04-08 23:44:49 +0200

    Prevent potential bug

--- project/src/js/ui/ai/replay/replayCtrl.js
+++ project/src/js/ui/ai/replay/replayCtrl.js
@@ -34,7 +34,7 @@ module.exports = function(root, situations, ply) {
 
   this.jump = function(ply) {
     this.root.chessground.cancelMove();
-    if (this.situation().turnColor !== this.root.data.player.color) ply++;
+    if (this.situation() && this.situation().turnColor !== this.root.data.player.color) ply++;
     if (this.ply === ply || ply < 0 || ply >= this.situations.length) return;
     this.ply = ply;
     this.apply();

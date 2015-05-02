5747b5353fbadc8a8dfa34b78cbc97dd3fa38c81
Vincent Velociter <vincent.velociter@gmail.com>
2015-05-02 21:09:28 +0200

    User profile

--- project/src/js/ui/user/userView.js
+++ project/src/js/ui/user/userView.js
@@ -3,17 +3,39 @@ var widgets = require('../widget/common');
 var perf = require('../widget/perf');
 var layout = require('../layout');
 var menu = require('../menu');
+var moment = window.moment;
+var i18n = require('../../i18n');
+
+function renderProfile(user) {
+  if (user.profile) {
+    var fullname = (user.profile.firstName || user.profile.lastName) ?
+      user.profile.firstName + ' ' + user.profile.lastName :
+      '';
+
+    return m('div.profile', [
+      m('h3.fullname', fullname),
+      m('br'),
+      m('p.bio', m('em', user.profile.bio)),
+      m('br'),
+      m('p.location', user.profile.location + ', ' + user.profile.countryName),
+    ]);
+  } else
+    return null;
+}
 
 function renderHeader(user) {
-  var fullname = (user.profile.firstName || user.profile.lastName) ?
-    user.profile.firstName + ' ' + user.profile.lastName :
-    '';
-
   var header = [
-    m('div.profile_bio', [
-      m('h3.fullname', fullname),
-      m('p.bio', m('em', user.profile.bio)),
-      m('p.location', user.profile.location)
+    m('div.userInfos', [
+      renderProfile(user),
+      m('p.memberSince', i18n('memberSince') + ' ' + moment(user.createdAt).format('LL')),
+      user.seenAt ?
+        m('p.lastSeen', i18n('lastLogin') + ' ' + moment(user.seenAt).format('LLL')) :
+        null,
+      m('br'),
+      user.playTime ? [
+        m('p.playTime', 'Time spent playing: ' + moment.duration(user.playTime.total, 'seconds').humanize()),
+        user.playTime.tv > 0 ? m('p.onTv', 'Time on TV: ' + moment.duration(user.playTime.tv, 'seconds').humanize()) : null
+      ] : null
     ])
   ];
 
@@ -29,8 +51,8 @@ function renderRatings(user) {
 module.exports = function(ctrl) {
   var user = ctrl.getUserData();
   var header = utils.partialf(widgets.header, null,
-      widgets.backButton(user.username ? user.username : '')
-    );
+    widgets.backButton(user.username ? user.username : '')
+  );
 
   var profile = function() {
     return [

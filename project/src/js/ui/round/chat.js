e74d15b928744da2f8cb115a26f53c3220580810
Vincent Velociter <vincent.velociter@gmail.com>
2015-09-21 11:27:35 +0200

    Revert "Autolink msg in chat"
    This reverts commit e323066acebd874c3d06127dc3265d20d3e59b64.

--- project/src/js/ui/round/chat.js
+++ project/src/js/ui/round/chat.js
@@ -4,7 +4,6 @@ import storage from '../../storage';
 import gameApi from '../../lichess/game';
 import backbutton from '../../backbutton';
 import socket from '../../socket';
-import Autolinker from 'autolinker';
 import m from 'mithril';
 
 export default {
@@ -141,7 +140,7 @@ export default {
                 'close_balloon': closeBalloon
               })
             }, [
-              m.trust(Autolinker.link(msg.t))
+              m.trust(msg.t)
             ]);
           }))
         ]),

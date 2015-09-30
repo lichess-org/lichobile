1e825a7eb002c1bb15d53eb3548f3d8323eef825
Vincent Velociter <vincent.velociter@gmail.com>
2015-09-30 19:25:44 +0200

    Show socket disconnection during seek

--- project/src/js/ui/seek.js
+++ project/src/js/ui/seek.js
@@ -1,11 +1,12 @@
 import * as utils from '../utils';
 import helper from './helper';
 import layout from './layout';
-import { header, viewOnlyBoardContent } from './shared/common';
+import { header, viewOnlyBoardContent, loader } from './shared/common';
 import popupWidget from './shared/popup';
 import * as xhr from '../xhr';
 import i18n from '../i18n';
 import socket from '../socket';
+import signals from '../signals';
 import m from 'mithril';
 
 var nbPlaying = 0;
@@ -14,18 +15,33 @@ var seek = {};
 
 seek.controller = function() {
 
+  var connectedWS = true;
   var hookId;
 
   helper.analyticsTrackView('Seek');
 
-  var createHook = function() {
+  function createHook() {
     if (hookId) return; // hook already created!
     xhr.seekGame().then(function(data) {
       hookId = data.hook.id;
     }, function(error) {
       utils.handleXhrError(error);
     });
-  };
+  }
+
+  function onConnected() {
+    var wasOff = !connectedWS;
+    connectedWS = true;
+    if (wasOff) m.redraw();
+  }
+
+  function onDisconnected() {
+    var wasOn = connectedWS;
+    connectedWS = false;
+    if (wasOn) setTimeout(function() {
+      m.redraw();
+    }, 500);
+  }
 
   xhr.lobby(true).then(data => {
     socket.createLobby(data.lobby.version, createHook, {
@@ -46,14 +62,19 @@ seek.controller = function() {
   }
 
   document.addEventListener('backbutton', cancel, false);
+  signals.socket.connected.add(onConnected);
+  signals.socket.disconnected.add(onDisconnected);
   window.plugins.insomnia.keepAwake();
 
   return {
     cancel: cancel,
+    isConnectedWS: () => connectedWS,
 
     onunload: function() {
       socket.destroy();
       document.removeEventListener('backbutton', cancel, false);
+      signals.socket.connected.remove(onConnected);
+      signals.socket.disconnected.remove(onDisconnected);
       window.plugins.insomnia.allowSleepAgain();
     }
   };
@@ -66,7 +87,10 @@ seek.view = function(ctrl) {
       m('div', i18n('waitingForOpponent') + '...'),
       function() {
         return m('div.seek_real_time', [
-          m('div.nb_players', i18n('nbConnectedPlayers', nbPlaying || '?')),
+          m('div.nb_players', ctrl.isConnectedWS() ?
+            i18n('nbConnectedPlayers', nbPlaying || '?') :
+            m('div', [i18n('reconnecting'), loader])
+          ),
           m('br'),
           m('br'),
           m('button[data-icon=L]', {

92cba9eb8995ba85d0f65e8cdf7b39cd693a8c1b
Vincent Velociter <vincent.velociter@gmail.com>
2015-12-13 00:31:53 +0100

    Wip on push: adapt to server api

--- project/src/js/push.js
+++ project/src/js/push.js
@@ -20,8 +20,9 @@ export default {
     push.on('registration', function(data) {
       if (session.isConnected()) {
         // we won't try to register again on failure for now
+        const platform = window.cordova.platformId;
         const deviceId = encodeURIComponent(data.registrationId);
-        request(`/mobile/register/${deviceId}`);
+        request(`/mobile/register/${platform}/${deviceId}`);
       }
     });
 
@@ -34,8 +35,10 @@ export default {
       }
       // if background we go to the game
       else {
-        if (data.additionalData.gameId)
-          m.route(`/game/${data.additionalData.gameId}`);
+        if (data.additionalData && data.additionalData.userData) {
+          // should we have fullId here?
+          m.route(`/game/${data.additionalData.userData.gameId}`);
+        }
       }
     });
   }

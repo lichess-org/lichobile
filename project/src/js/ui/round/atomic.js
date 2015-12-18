b083f6f2c7f274458f8e084efce0df1526614d64
Vincent Velociter <vincent.velociter@gmail.com>
2015-12-18 23:11:41 +0100

    Fix chessground mobile-version

--- project/src/js/ui/round/atomic.js
+++ project/src/js/ui/round/atomic.js
@@ -1,16 +1,16 @@
-var util = require('chessground').util;
+import { util } from 'chessground-mobile';
 
 function capture(ctrl, key) {
-  var exploding = [];
-  var diff = {};
-  var orig = util.key2pos(key);
-  for (var x = -1; x < 2; x++) {
-    for (var y = -1; y < 2; y++) {
-      var k = util.pos2key([orig[0] + x, orig[1] + y]);
+  const exploding = [];
+  const diff = {};
+  const orig = util.key2pos(key);
+  for (let x = -1; x < 2; x++) {
+    for (let y = -1; y < 2; y++) {
+      const k = util.pos2key([orig[0] + x, orig[1] + y]);
       if (k) {
         exploding.push(k);
-        var explodes = ctrl.chessground.data.pieces[k] && (
-          k === key || ctrl.chessground.data.pieces[k].role !== 'pawn')
+        const explodes = ctrl.chessground.data.pieces[k] && (
+          k === key || ctrl.chessground.data.pieces[k].role !== 'pawn');
         if (explodes) diff[k] = null;
       }
     }
@@ -21,12 +21,12 @@ function capture(ctrl, key) {
 
 // needs to explicitly destroy the capturing pawn
 function enpassant(ctrl, key, color) {
-  var pos = util.key2pos(key);
-  var pawnPos = [pos[0], pos[1] + (color === 'white' ? -1 : 1)];
+  const pos = util.key2pos(key);
+  const pawnPos = [pos[0], pos[1] + (color === 'white' ? -1 : 1)];
   capture(ctrl, util.pos2key(pawnPos));
 }
 
-module.exports = {
-  capture: capture,
-  enpassant: enpassant
+export default {
+  capture,
+  enpassant
 };

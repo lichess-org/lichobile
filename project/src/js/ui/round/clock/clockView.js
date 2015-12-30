7caf836d847453b056cf532a44ec686dc1bb7f68
Vincent Velociter <vincent.velociter@gmail.com>
2015-12-30 14:44:04 +0100

    Even better clock

--- project/src/js/ui/round/clock/clockView.js
+++ project/src/js/ui/round/clock/clockView.js
@@ -4,20 +4,23 @@ function prefixInteger(num, length) {
   return (num / Math.pow(10, length)).toFixed(length).substr(2);
 }
 
+const sepHigh = '<seph>:</seph>';
+const sepLow = '<sepl>:</sepl>';
+
 export function formatClockTime(ctrl, time, isRunning) {
   var date = new Date(time);
   var minutes = prefixInteger(date.getUTCMinutes(), 2);
   var seconds = prefixInteger(date.getUTCSeconds(), 2);
   var tenths = Math.floor(date.getUTCMilliseconds() / 100);
-  let pulse = (isRunning && tenths > 5) ? ':' : isRunning ? ' ' : ':';
+  let pulse = (isRunning && tenths < 5) ? sepLow : sepHigh;
 
   if (ctrl.data.showTenths && time < 10000) {
-    return minutes + pulse + seconds + '.' + tenths;
+    return seconds + '<tens><seph>.</seph>' + tenths + '</tens>';
   }
 
   if (time >= 3600000) {
     let hours = prefixInteger(date.getUTCHours(), 1);
-    return hours + ':' + minutes + pulse + seconds;
+    return hours + sepHigh + minutes + pulse + seconds;
   }
 
   return minutes + pulse + seconds;
@@ -33,7 +36,7 @@ export function view(ctrl, color, runningColor) {
     emerg: time < ctrl.data.emerg
   });
   function cConfig(el) {
-    requestAnimationFrame(() => el.textContent = formatClockTime(ctrl, time * 1000, isRunning));
+    requestAnimationFrame(() => el.innerHTML = formatClockTime(ctrl, time * 1000, isRunning));
   }
   return (
     <div id={'clock_' + color} className={className} config={cConfig} />

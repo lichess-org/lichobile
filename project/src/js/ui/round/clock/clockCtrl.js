d763d16e6551226484527ea66b598fc3c4c5bb05
Vincent Velociter <vincent.velociter@gmail.com>
2016-01-05 10:42:54 +0100

    Use custom mithril; tweak clock update

--- project/src/js/ui/round/clock/clockCtrl.js
+++ project/src/js/ui/round/clock/clockCtrl.js
@@ -44,7 +44,7 @@ export default function ctrl(data, outOfTime, soundColor) {
     const time = this.data[color] * 1000;
     const el = this.els[color];
 
-    if (el) requestAnimationFrame(() => el.textContent = formatClockTime(this, time, true));
+    if (el) el.textContent = formatClockTime(this, time, true);
 
     if (this.data[color] < this.data.emerg && !this.emerg[color]) {
       this.emerg[color] = true;

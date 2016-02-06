55fac2c8bb32eed0252c162bbd02a09355bdadcc
Vincent Velociter <vincent.velociter@gmail.com>
2016-02-06 10:25:18 +0100

    Remove every challenge related thing in game

--- project/src/js/ui/game/gameCtrl.js
+++ project/src/js/ui/game/gameCtrl.js
@@ -1,10 +1,8 @@
 import session from '../../session';
-import helper from '../helper';
-import { hasNetwork, handleXhrError, backHistory, getOfflineGameData, saveOfflineGameData, removeOfflineGameData } from '../../utils';
-import { game as gameXhr, joinChallenge, cancelChallenge } from '../../xhr';
+import { hasNetwork, handleXhrError, getOfflineGameData, saveOfflineGameData, removeOfflineGameData } from '../../utils';
+import { game as gameXhr } from '../../xhr';
 import storage from '../../storage';
 import roundCtrl from '../round/roundCtrl';
-import gameStatus from '../../lichess/status';
 import gameApi from '../../lichess/game';
 import variantApi from '../../lichess/variant';
 import socket from '../../socket';
@@ -14,12 +12,8 @@ import i18n from '../../i18n';
 import m from 'mithril';
 
 export default function controller() {
-  const isAwaitingInvite = m.prop(false);
-  const isAwaitingChallenge = m.prop(false);
-  const isJoinable = m.prop(false);
   var gameData;
   var round;
-  var challengeIntervalID;
 
   if (hasNetwork()) {
     gameXhr(m.route.param('id'), m.route.param('color'), !!gamesMenu.lastJoined).then(function(data) {
@@ -29,39 +23,7 @@ export default function controller() {
         window.plugins.toast.show(i18n('unsupportedVariant', data.game.variant.name), 'short', 'center');
         m.route('/');
       }
-      // joinable means it's a game opened from an url scheme (url invitation from
-      // a friend)
-      else if (data.game.joinable) {
-        helper.analyticsTrackView('Join url invitation');
-        isJoinable(true);
-      }
-      // status created means user has sent an url invitation or challenge, and is
-      // waiting for friend to join
-      else if (data.game.status.id === gameStatus.ids.created) {
-        socket.createAwait(data.url.socket, data.player.version, {
-          redirect: e => m.route('/game/' + e.id),
-          declined: () => {
-            window.plugins.toast.show('Challenge declined', 'short', 'center');
-            backHistory();
-          }
-        });
-        // userId param means it's a challenge, otherwise it's an invitation with url
-        if (m.route.param('userId')) {
-          helper.analyticsTrackView('Waiting for challenge acceptance');
-          isAwaitingChallenge(true);
-          // to keep challenge open
-          challengeIntervalID = setInterval(() => {
-            socket.send('challenge', m.route.param('userId'));
-          }, 1500);
-          window.plugins.insomnia.keepAwake();
-        } else {
-          helper.analyticsTrackView('Waiting for URL invitation acceptance');
-          isAwaitingInvite(true);
-        }
-      }
-      // if not joinable or created, it means the game is started, so let's play!
       else {
-        session.refresh();
 
         if (gameApi.isPlayerPlaying(data) &&
         gameApi.nbMoves(data, data.player.color) === 0) {
@@ -83,7 +45,12 @@ export default function controller() {
         }
 
         if (gameData.game.speed === 'correspondence') {
-          saveOfflineGameData(m.route.param('id'), gameData);
+          session.refresh();
+          if (!gameApi.playable(gameData)) {
+            removeOfflineGameData(m.route.param('id'));
+          } else {
+            saveOfflineGameData(m.route.param('id'), gameData);
+          }
         }
 
       }
@@ -111,23 +78,10 @@ export default function controller() {
         round.onunload();
         round = null;
       }
-      if (challengeIntervalID) clearInterval(challengeIntervalID);
       socket.destroy();
-      window.plugins.insomnia.allowSleepAgain();
     },
     getRound: function() {
       return round;
-    },
-    isJoinable,
-    isAwaitingInvite,
-    isAwaitingChallenge,
-    joinChallenge: id => joinChallenge(id).then(data =>
-      m.route('/game' + data.url.round)
-    ),
-    cancelChallenge: () => {
-      cancelChallenge(gameData.url.round);
-      if (challengeIntervalID) clearInterval(challengeIntervalID);
-      backHistory();
     },
     getData: function() {
       return gameData;
--- project/src/js/ui/game/gameView.js
+++ project/src/js/ui/game/gameView.js
@@ -1,21 +1,14 @@
-import session from '../../session';
 import roundView from '../round/view/roundView';
-import gameApi from '../../lichess/game';
 import gamesMenu from '../gamesMenu';
-import loginModal from '../loginModal';
 import layout from '../layout';
 import * as utils from '../../utils';
-import helper from '../helper';
 import { connectingHeader, viewOnlyBoardContent, header as headerWidget } from '../shared/common';
-import popupWidget from '../shared/popup';
-import i18n from '../../i18n';
-import m from 'mithril';
 
 export default function view(ctrl) {
   if (ctrl.getRound()) return roundView(ctrl.getRound());
 
   var pov = gamesMenu.lastJoined;
-  var header, board, overlay;
+  var header, board;
 
   if (pov) {
     header = connectingHeader;
@@ -27,120 +20,5 @@ export default function view(ctrl) {
     board = viewOnlyBoardContent;
   }
 
-  if (ctrl.isJoinable()) overlay = joinOverlay(ctrl);
-  else if (ctrl.isAwaitingInvite()) overlay = awaitInviteOverlay(ctrl);
-  else if (ctrl.isAwaitingChallenge()) overlay = awaitChallengeOverlay(ctrl);
-
-  return layout.board(header, board, overlay);
-}
-
-function gameInfos(gameData) {
-  const mode = gameData.game.rated ? i18n('rated') : i18n('casual');
-  return (
-    <div className="gameInfos">
-      <p className="explanation small">{`${i18n('variant')}: ${gameData.game.variant.name}`}</p>
-      <p className="time small" data-icon="p">{gameApi.time(gameData)}</p>
-      <p className="mode small">{`${i18n('mode')}: ${mode}`}</p>
-    </div>
-  );
-}
-
-function joinOverlay(ctrl) {
-  var data = ctrl.getData();
-  var opp = data.opponent.user;
-  var joinDom;
-  if (data.game.rated && !session.isConnected()) {
-    joinDom = m('div.error', [
-      i18n('thisGameIsRated'), m('br'), m('br'), i18n('mustSignInToJoin'),
-      m('div.go_or_cancel', [
-        m('button.binary_choice[data-icon=E]', {
-          config: helper.ontouch(loginModal.open)
-        }, i18n('signIn')),
-        m('button.binary_choice[data-icon=L]', {
-          config: helper.ontouch(utils.backHistory)
-        }, i18n('cancel'))
-      ])
-    ]);
-  } else {
-    joinDom = m('div.go_or_cancel', [
-      m('button.binary_choice[data-icon=E]', {
-          config: helper.ontouch(utils.f(ctrl.joinChallenge, data.game.id))
-      }, i18n('join')),
-      m('button.binary_choice[data-icon=L]', {
-        config: helper.ontouch(utils.backHistory)
-      }, i18n('cancel'))
-    ]);
-  }
-
-  return function() {
-    return popupWidget(
-      'join_url_challenge',
-      () => opp ? opp.username : 'Anonymous',
-      function() {
-        return m('div.infos', [
-          gameInfos(data),
-          m('br'),
-          joinDom
-        ]);
-      },
-      true
-    );
-  };
-}
-
-function awaitInviteOverlay(ctrl) {
-  var data = ctrl.getData();
-
-  return function() {
-    return popupWidget(
-      'await_url_challenge',
-      null,
-      function() {
-        return m('div.infos', [
-          m('p.explanation', i18n('toInviteSomeoneToPlayGiveThisUrl')),
-          m('input.lichess_game_url', {
-            value: gameApi.publicUrl(data),
-            readonly: true
-          }),
-          m('p.explanation.small', i18n('theFirstPersonToComeOnThisUrlWillPlayWithYou')),
-          m('div.go_or_cancel.clearfix', [
-            m('button.binary_choice[data-icon=E]', {
-              config: helper.ontouch(function() {
-                window.plugins.socialsharing.share(null, null, null, gameApi.publicUrl(data));
-              })
-            }, i18n('shareGameURL')),
-            m('button.binary_choice[data-icon=L]', {
-              config: helper.ontouch(ctrl.cancelChallenge)
-            }, i18n('cancel'))
-          ]),
-          m('br'),
-          gameInfos(data)
-        ]);
-      },
-      true
-    );
-  };
-}
-
-function awaitChallengeOverlay(ctrl) {
-  function popupContent() {
-    return (
-      <div className="infos">
-        <div className="user">{m.route.param('userId')}</div>
-        <br />
-        <div className="loader"><span data-icon="U" /></div>
-        <br />
-        <p>{i18n('waitingForOpponent')}</p>
-        <button data-icon="L" config={helper.ontouch(ctrl.cancelChallenge)}>
-          {i18n('cancel')}
-        </button>
-        <br />
-        {gameInfos(ctrl.getData())}
-      </div>
-    );
-  }
-
-  return function() {
-    return popupWidget('await_url_challenge', () => i18n('challengeToPlay'), popupContent, true);
-  };
+  return layout.board(header, board);
 }

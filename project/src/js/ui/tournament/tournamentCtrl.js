729e73e920ddfd4beb67d4558e7dc17a1fb648df
Mark Henle <freefal67@gmail.com>
2016-02-18 08:14:21 -0500

    Make tournament a private variable of controller

--- project/src/js/ui/tournament/tournamentCtrl.js
+++ project/src/js/ui/tournament/tournamentCtrl.js
@@ -5,9 +5,28 @@ import socketHandler from './socketHandler';
 import helper from '../helper';
 import m from 'mithril';
 
-const tournament = m.prop([]);
+
 
 export default function controller() {
+  const tournament = m.prop([]);
+  function reload (data) {
+    tournament(data);
+    if(data.socketVersion)
+      socket.setVersion(data.socketVersion);
+    m.redraw();
+  }
+
+  function tick () {
+    let data = tournament();
+    if(data.secondsToStart && data.secondsToStart > 0)
+      data.secondsToStart--;
+
+    if(data.secondsToFinish && data.secondsToFinish > 0)
+      data.secondsToFinish--;
+
+    m.redraw();
+  }
+
   let id = m.route.param('id');
   helper.analyticsTrackView('Tournament ' + id);
 
@@ -30,22 +49,4 @@ export default function controller() {
 
   socket.createTournament(tournament().socketVersion, id, socketHandler(returnVal));
   return returnVal;
-}
-
-function reload (data) {
-  tournament(data);
-  if(data.socketVersion)
-    socket.setVersion(data.socketVersion);
-  m.redraw();
-}
-
-function tick () {
-  let data = tournament();
-  if(data.secondsToStart && data.secondsToStart > 0)
-    data.secondsToStart--;
-
-  if(data.secondsToFinish && data.secondsToFinish > 0)
-    data.secondsToFinish--;
-
-  m.redraw();
 }

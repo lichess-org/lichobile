0e62b6fa08d4a620f7f403d8cac887708f7fa35f
Vincent Velociter <vincent.velociter@gmail.com>
2016-02-22 11:36:31 +0100

    Fix games menu bug

--- project/src/js/ui/gamesMenu.js
+++ project/src/js/ui/gamesMenu.js
@@ -159,7 +159,7 @@ function renderGame(g, cDim, cardStyle) {
 function renderIncomingChallenge(c, cDim, cardStyle) {
   const mode = c.rated ? i18n('rated') : i18n('casual');
   const timeAndMode = utils.challengeTime(c) + ', ' + mode;
-  const mark = c.destUser.provisional ? '?' : '';
+  const mark = c.challenger.provisional ? '?' : '';
   const playerName = `${c.challenger.id} (${c.challenger.rating}${mark})`;
 
   return (

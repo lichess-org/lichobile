c753222c217c4607fcbf2f9de0be9884c4f1990d
Vincent Velociter <vincent.velociter@gmail.com>
2016-03-27 09:49:28 +0200

    Improve stockfish engine config

--- project/src/js/ui/ai/engine.js
+++ project/src/js/ui/ai/engine.js
@@ -1,21 +1,24 @@
-const levels = {
-  1: [20, 1],
-  2: [50, 2],
-  3: [100, 3],
-  4: [200, 4],
-  5: [300, 6],
-  6: [800, 8],
-  7: [2000, 12],
-  8: [4000, 20]
+const maxMoveTime = 8000;
+const maxSkill = 20;
+const levelToDepth = {
+  1: 1,
+  2: 1,
+  3: 2,
+  4: 3,
+  5: 5,
+  6: 8,
+  7: 13,
+  8: 21
 };
 
 export default function(ctrl) {
-  let analysisDuration;
+  let level = 1;
   const bestmoveRegExp = /^bestmove (\w{4})/;
 
   return {
     init(cb) {
       window.Stockfish.init(function() {
+
         window.Stockfish.output(function(msg) {
           console.log(msg);
           const bestmoveRegExpMatch = msg.match(bestmoveRegExp);
@@ -23,21 +26,41 @@ export default function(ctrl) {
             ctrl.onEngineSearch(bestmoveRegExpMatch[1]);
           }
         });
+
+        setOption('Ponder', 'false');
+
         cb();
       });
     },
 
     search(fen) {
       window.Stockfish.cmd(`position fen ${fen}`);
-      window.Stockfish.cmd(`go movetime ${analysisDuration}`);
+      window.Stockfish.cmd(`go movetime ${moveTime(level)} depth ${depth(level)}`);
     },
 
-    setLevel(level) {
-      analysisDuration = levels[level][0];
+    setLevel(l) {
+      level = l;
+      setOption('Skill Level', skill(level));
     },
 
     exit() {
       window.Stockfish.exit();
     }
   };
+}
+
+function setOption(name, value) {
+  window.Stockfish.cmd(`setoption name ${name} value ${value}`);
+}
+
+function moveTime(level) {
+  return level * maxMoveTime / 8;
+}
+
+function skill(level) {
+  return Math.round((level - 1) * (maxSkill / 7));
+}
+
+function depth(level) {
+  return levelToDepth[level];
 }

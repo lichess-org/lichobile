b6bcd4674c273b41e268474a5aef64276395bcc4
Vincent Velociter <vincent.velociter@gmail.com>
2016-04-11 10:11:52 +0200

    Add deep link test page

--- project/src/js/main.js
+++ project/src/js/main.js
@@ -19,20 +19,17 @@ import backbutton from './backbutton';
 import socket from './socket';
 import push from './push';
 import routes from './routes';
+import deepLinks from './deepLinks';
 
 var triedToLogin = false;
 
 function main() {
 
   routes.init();
+  deepLinks.init();
 
   // cache viewport dims
   helper.viewportDim();
-
-  // open games from external links with url scheme
-  window.handleOpenURL = function(url) {
-    setTimeout(onUrlOpen.bind(undefined, url), 0);
-  };
 
   // iOs needs this to auto-rotate
   window.shouldRotateToOrientation = function() {
@@ -72,31 +69,6 @@ function main() {
     window.navigator.splashscreen.hide();
     xhrStatus();
   }, 500);
-}
-
-function onUrlOpen(url) {
-  const uris = [
-    {
-      reg: /^lichess:\/\/training\/(\d+)/,
-      ctrl: (id) => m.route(`/training/${id}`)
-    },
-    {
-      reg: /^lichess:\/\/(\w+)\/?(black|white)?/,
-      ctrl: (gameId, color) => {
-        let route = '/game/' + gameId;
-        if (color) route += ('/' + color);
-        m.route(route);
-      }
-    }
-  ];
-  for (var i = 0; i <= uris.length; i++) {
-    const r = uris[i];
-    const parsed = r.reg.exec(url);
-    if (parsed !== null) {
-      r.ctrl.apply(null, parsed.slice(1));
-      break;
-    }
-  }
 }
 
 function onResize() {

f913208e385777f01f8b073f888992d147f07dc0
Vincent Velociter <vincent.velociter@gmail.com>
2016-07-14 10:27:08 +0200

    Revert "Forgot to add param here"
    This reverts commit f265e21f058e3cbe00053f92347004c6f61e066d.

--- project/src/js/ui/helper/index.js
+++ project/src/js/ui/helper/index.js
@@ -132,7 +132,7 @@ function findParentBySelector(el, selector) {
   return cur;
 }
 
-function ontouch(tapHandler, holdHandler, repeatHandler, scrollX, scrollY, touchEndFeedback, shouldPreventDefault) {
+function ontouch(tapHandler, holdHandler, repeatHandler, scrollX, scrollY, touchEndFeedback) {
   return function(el, isUpdate) {
     if (!isUpdate) {
       ButtonHandler(el,
@@ -148,8 +148,7 @@ function ontouch(tapHandler, holdHandler, repeatHandler, scrollX, scrollY, touch
         repeatHandler,
         scrollX,
         scrollY,
-        touchEndFeedback,
-        shouldPreventDefault
+        touchEndFeedback
       );
     }
   };

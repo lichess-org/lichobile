b7c140faaa0e06c2dd0001c41f46bb9dd5438f0b
Vincent Velociter <vincent.velociter@gmail.com>
2016-07-27 13:59:39 +0200

    Fix training chart

--- project/src/js/ui/training/menu.js
+++ project/src/js/ui/training/menu.js
@@ -45,45 +45,23 @@ export default {
   }
 };
 
-function renderDifficulty(ctrl) {
-  const opts = ctrl.data.difficulty.choices.map(c => ['difficulty' + c[1], c[0]]);
-  return m('div.select_input.puzzleDifficulty',
-    formWidgets.renderSelect('level', 'difficulty', opts,
-      () => ctrl.data.difficulty.current, false, ctrl.setDifficulty)
-  );
-}
-
 export function renderUserInfos(ctrl) {
   return [
     m('p', m.trust(i18n('yourPuzzleRatingX', `<strong>${ctrl.data.user.rating}</strong>`))),
     helper.isPortrait() ? m('br') : null,
-    ctrl.data.user.history ? m('canvas#puzzleChart', {
-      width: '240px',
-      height: '150px',
-      oncreate: function(vnode) {
-        const hash = ctrl.data.user.history.join('') + (helper.isPortrait() ? 'portrait' : 'landscape');
+    ctrl.data.user.history ? m('canvas', {
+      oncreate(vnode) {
+        const ctx = vnode.dom.getContext('2d');
+        console.log(vnode.dom);
+        drawChart(ctrl, ctx);
+        vnode.state.hash = chartHash(ctrl);
+      },
+      onupdate(vnode) {
+        const hash = chartHash(ctrl);
         if (hash === vnode.state.hash) return;
         vnode.state.hash = hash;
-        const ctx = document.getElementById('puzzleChart').getContext('2d');
-        if (helper.isPortrait()) {
-          ctx.canvas.width = ctx.canvas.parentElement.offsetWidth - 20;
-        } else {
-          ctx.canvas.width = ctx.canvas.parentElement.offsetWidth;
-          ctx.canvas.height = ctx.canvas.parentElement.offsetHeight - 20;
-        }
-        new Chart(ctx).Line({
-          labels: ctrl.data.user.history.map(() => ''),
-          datasets: [
-            {
-              data: ctrl.data.user.history,
-              fillColor: 'rgba(196, 168, 111, 0.4)',
-              strokeColor: 'rgba(196, 168, 111, 0.8)'
-            }
-          ]
-        }, {
-          pointDot: false,
-          scaleShowGridLines: false
-        });
+        const ctx = vnode.dom.getContext('2d');
+        drawChart(ctrl, ctx);
       }
     }) : null
   ];
@@ -100,6 +78,43 @@ export function renderSigninBox() {
     m('p', i18n('trainingSignupExplanation'))
   ]);
 }
+
+function chartHash(ctrl) {
+  return ctrl.data.user.history.join('') + (helper.isPortrait() ? 'portrait' : 'landscape');
+}
+
+function drawChart(ctrl, ctx) {
+  const canvas = ctx.canvas;
+  if (helper.isPortrait()) {
+    canvas.width = canvas.style.width = canvas.parentElement.offsetWidth - 20;
+    canvas.height = canvas.style.height = 150;
+  } else {
+    canvas.width = canvas.style.width = canvas.parentElement.offsetWidth;
+    canvas.height = canvas.style.height = canvas.parentElement.offsetHeight - 20;
+  }
+  new Chart(ctx).Line({
+    labels: ctrl.data.user.history.map(() => ''),
+    datasets: [
+      {
+        data: ctrl.data.user.history,
+        fillColor: 'rgba(196, 168, 111, 0.4)',
+        strokeColor: 'rgba(196, 168, 111, 0.8)'
+      }
+    ]
+  }, {
+    pointDot: false,
+    scaleShowGridLines: false
+  });
+}
+
+function renderDifficulty(ctrl) {
+  const opts = ctrl.data.difficulty.choices.map(c => ['difficulty' + c[1], c[0]]);
+  return m('div.select_input.puzzleDifficulty',
+    formWidgets.renderSelect('level', 'difficulty', opts,
+      () => ctrl.data.difficulty.current, false, ctrl.setDifficulty)
+  );
+}
+
 
 function renderTrainingMenu(ctrl) {
   if (ctrl.data.user) {

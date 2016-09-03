06ae7db9d52613cf3d292265402019e362fa0895
Vincent Velociter <vincent.velociter@gmail.com>
2016-09-03 17:59:02 +0200

    Fix build

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -58,7 +58,10 @@ function buildScripts(src, dest, mode) {
 
   return browserify(src + '/js/main.ts', opts)
     .plugin(tsify, tsConfig)
-    .transform(babelify, {extensions: ['.tsx', '.ts', '.js', '.jsx']}, {presets: ['es2015']})
+    .transform(babelify, {
+      extensions: ['.tsx', '.ts', '.js', '.jsx'],
+      presets: ['es2015']
+    })
     .bundle()
     .on('error', function(error) { gutil.log(gutil.colors.red(error.message)); })
     .pipe(source('app.js'))
@@ -87,7 +90,10 @@ gulp.task('watch-scripts', function() {
   var bundleStream = watchify(
     browserify('./src/js/main.ts', opts)
     .plugin(tsify, tsConfig)
-    .transform(babelify, {extensions: ['.tsx', '.ts', '.js', '.jsx']}, {presets: ['es2015']})
+    .transform(babelify, {
+      extensions: ['.tsx', '.ts', '.js', '.jsx'],
+      presets: ['es2015']
+    })
   );
 
   function rebundle() {

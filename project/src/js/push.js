3dd398a414e5955e0d92cc7d725d585afde4bd9e
Vincent Velociter <vincent.velociter@gmail.com>
2016-09-04 16:14:28 +0200

    Onesignal push

--- project/src/js/push.js
+++ project/src/js/push.js
@@ -1,114 +1,60 @@
-import router from './router';
-import redraw from './utils/redraw';
-import session from './session';
 import settings from './settings';
-import i18n from './i18n';
-import { lightPlayerName } from './utils';
-import { fetchText } from './http';
+import session from './session';
+import router from './router';
 import challengesApi from './lichess/challenges';
+import { fetchText } from './http';
 
-let push;
+function notificationOpenedCallback({ additionalData, isActive }) {
+  if (additionalData.userData) {
+    if (isActive) {
+      switch (additionalData.userData.type) {
+        case 'gameMove':
+        case 'gameFinish':
+          session.refresh();
+          break;
+      }
+    } else {
+      switch (additionalData.userData.type) {
+        case 'challengeCreate':
+          router.set(`/challenge/${additionalData.userData.challengeId}`);
+          break;
+        case 'challengeAccept':
+          challengesApi.refresh();
+          router.set(`/game/${additionalData.userData.challengeId}`);
+          break;
+        case 'gameMove':
+          router.set(`/game/${additionalData.userData.fullId}`);
+          break;
+        case 'gameFinish':
+          router.set(`/game/${additionalData.userData.fullId}`);
+          break;
+      }
+    }
+  }
+}
 
 export default {
   register() {
 
-    if (window.cordova.platformId === 'android' && settings.general.notifications.allow()) {
+    if (settings.general.notifications.allow()) {
 
-      push = window.PushNotification.init({
-        android: {
-          senderID: window.lichess.gcmSenderId,
-          sound: settings.general.notifications.sound(),
-          vibrate: settings.general.notifications.vibrate(),
-          clearNotifications: true
-        },
-        ios: {
-          sound: true,
-          alert: true,
-          badge: true
-        }
+      window.plugins.OneSignal.init(
+        '2d12e964-92b6-444e-9327-5b2e9a419f4c',
+        {googleProjectNumber: '1050901934956'},
+        notificationOpenedCallback
+      );
+
+      window.plugins.OneSignal.getIds(({ userId }) => {
+        fetchText(`/mobile/register/onesignal/${userId}`, {
+          method: 'POST'
+        });
       });
 
-      push.on('registration', function(data) {
-        if (session.isConnected()) {
-          // we won't try to register again on failure for now
-          const platform = window.cordova.platformId;
-          const deviceId = encodeURIComponent(data.registrationId);
-          fetchText(`/mobile/register/${platform}/${deviceId}`, {
-            method: 'POST'
-          });
-        }
-      });
-
-      push.on('notification', function(data) {
-        // if app was foreground we don't want to disturb too much so we'll
-        // just refresh nb of turns in board icon
-        const payload = data.additionalData;
-        if (payload) {
-          if (payload.foreground) {
-            // if foreground just refresh according data
-            if (payload.userData) {
-              switch (payload.userData.type) {
-                case 'challengeCreate':
-                  challengesApi.refresh().then(() => redraw());
-                  break;
-                case 'challengeAccept':
-                  Promise.all([
-                    challengesApi.refresh(),
-                    session.refresh()
-                  ])
-                  .then(() => {
-                    window.plugins.toast.show(
-                      i18n('userAcceptsYourChallenge', lightPlayerName(payload.userData.joiner)), 'long', 'top');
-                    redraw();
-                  });
-                  break;
-                case 'gameMove':
-                  session.refresh().then(v => {
-                    if (v) redraw();
-                  });
-                  break;
-                case 'gameFinish':
-                  session.refresh()
-                  .then(() => redraw());
-                  break;
-              }
-            }
-          }
-          // if background we go to the game or challenge
-          else if (payload.userData) {
-            switch (payload.userData.type) {
-              case 'challengeCreate':
-                router.set(`/challenge/${payload.userData.challengeId}`);
-                break;
-              case 'challengeAccept':
-                challengesApi.refresh();
-                router.set(`/game/${payload.userData.challengeId}`);
-                break;
-              case 'gameMove':
-                router.set(`/game/${payload.userData.fullId}`);
-                break;
-              case 'gameFinish':
-                router.set(`/game/${payload.userData.fullId}`);
-                break;
-            }
-          }
-        }
-      });
-
+      window.plugins.OneSignal.enableInAppAlertNotification(false);
     }
   },
 
   unregister() {
-    if (push) {
-      push.unregister(function() {
-        fetchText('/mobile/unregister', {
-          method: 'POST'
-        }).then(() => {
-          push = null;
-        });
-
-      }, console.log.bind(console));
-    }
+    fetchText('/mobile/unregister', { method: 'POST' });
   }
 };
-

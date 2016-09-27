a1d4ace0da11747ee91e779a56b76bddca1e7d7a
Vincent Velociter <vincent.velociter@gmail.com>
2016-09-27 12:41:49 +0200

    Fix training reload

--- project/src/js/ui/loginModal.js
+++ project/src/js/ui/loginModal.js
@@ -1,5 +1,7 @@
 import session from '../session';
+import redraw from '../utils/redraw';
 import socket from '../socket';
+import signals from '../signals';
 import push from '../push';
 import challengesApi from '../lichess/challenges';
 import * as utils from '../utils';
@@ -19,9 +21,11 @@ function submit(form) {
   if (!login || !pass) return false;
   window.cordova.plugins.Keyboard.close();
   return session.login(login, pass)
-  .then(function() {
+  .then(() => {
     loginModal.close();
     window.plugins.toast.show(i18n('loginSuccessful'), 'short', 'center');
+    signals.afterLogin.dispatch();
+    redraw();
     // reconnect socket to refresh friends...
     socket.connect();
     push.register();
--- project/src/js/ui/training/index.js
+++ project/src/js/ui/training/index.js
@@ -1,3 +1,4 @@
+import signals from '../../signals';
 import * as helper from '../helper';
 import oninit from './trainingCtrl';
 import view from './trainingView';
@@ -6,6 +7,7 @@ export default {
   oninit,
   oncreate: helper.viewFadeIn,
   onremove() {
+    signals.afterLogin.remove(this.retry);
     window.plugins.insomnia.allowSleepAgain();
   },
   view

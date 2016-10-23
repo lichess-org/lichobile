e0d73efc5e44e8f9a798757c9297ca4a3df95e21
Vincent Velociter <vincent.velociter@gmail.com>
2016-10-23 10:21:25 +0200

    Add hooks to allow autoinstall and remove unused permissions
    Closes #457

---
+++ app/hooks/after_prepare/remove_android_permissions.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+
+if (fs.existsSync('platforms/android')) {
+    const PERMISSIONS_TO_REMOVE = ['ACCESS_WIFI_STATE', 'WRITE_EXTERNAL_STORAGE'],
+        MANIFEST = 'platforms/android/AndroidManifest.xml',
+        manifestLines = fs.readFileSync(MANIFEST).toString().split('\n'),
+        newManifestLines = [];
+
+    const permissions_regex = PERMISSIONS_TO_REMOVE.join('|');
+
+    manifestLines.forEach(function(line) {
+        if (!line.match(permissions_regex)) {
+            newManifestLines.push(line);
+        }
+    });
+
+    fs.writeFileSync(MANIFEST, newManifestLines.join('\n'));
+}

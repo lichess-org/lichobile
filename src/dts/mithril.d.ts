ad8f10b0cb18a82d7f35ce791bd570f283b474bd
Vincent Velociter <vincent.velociter@gmail.com>
2016-11-16 22:42:44 +0100

    Update mithril

--- src/dts/mithril.d.ts
+++ src/dts/mithril.d.ts
@@ -71,16 +71,11 @@ declare namespace Mithril {
   interface Stream<T> {
     (): T;
     (value: T): this;
-    run(f: (current: T) => Stream<T> | T | void): Stream<T>;
-    run<U>(f: (current: T) => Stream<U> | U): Stream<U>;
     map(f: (current: T) => Stream<T> | T | void): Stream<T>;
     map<U>(f: (current: T) => Stream<U> | U): Stream<U>;
-    catch(f: (current: T) => T | void): Stream<T>;
-    catch<U>(f: (current: T) => U): Stream<U>;
     of(val?: T): Stream<T>;
     ap<U>(f: Stream<(value: T) => U>): Stream<U>;
     end: Stream<boolean>;
-    error: Stream<any>
   }
 
   type StreamCombiner<T> = (...streams: Stream<any>[]) => T

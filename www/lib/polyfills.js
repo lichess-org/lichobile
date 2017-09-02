b082de37aaaf97069c3d4c37239b6285191b8734
Vincent Velociter <vincent.velociter@gmail.com>
2017-09-02 14:18:11 +0200

    Add startsWith polyfill

--- www/lib/polyfills.js
+++ www/lib/polyfills.js
@@ -120,3 +120,10 @@ if (!String.prototype.includes) {
     return String.prototype.indexOf.apply(this, arguments) !== -1
   }
 }
+
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function (searchString, position) {
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+  };
+}

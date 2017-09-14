7bb38f8125fab6879cd285c4e28ce083c6553a19
Vincent Velociter <vincent.velociter@gmail.com>
2017-09-15 00:46:17 +0200

    More wip on acpl chart

--- src/js/ui/analyse/charts/acpl.ts
+++ src/js/ui/analyse/charts/acpl.ts
@@ -1,6 +1,6 @@
 import { select } from 'd3-selection'
 import { scaleLinear } from 'd3-scale'
-import { area } from 'd3-shape'
+import { area as d3Area } from 'd3-shape'
 
 import { AnalyseData } from '../../../lichess/interfaces/analyse'
 
@@ -12,39 +12,62 @@ interface Point {
 export default function AcplChart(element: SVGElement, aData: AnalyseData) {
   const graph = select(element)
   const graphData = makeSerieData(aData)
-  const data = graphData.map((p, i) => [i + 1, p.acpl] as [number, number])
   const margin = {top: 10, right: 20, bottom: 10, left: 20}
   const width = +graph.attr('width') - margin.left - margin.right
   const height = +graph.attr('height') - margin.top - margin.bottom
   const g = graph.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
-  const xvalues = data.map(d => d[0])
-  const scaleX = scaleLinear()
-  .domain([Math.min.apply(null, xvalues), Math.max.apply(null, xvalues)])
+  const x = scaleLinear()
+  .domain([0, graphData.length])
   .rangeRound([0, width])
 
-  const scaleY = scaleLinear()
+  const y = scaleLinear()
   .domain([-1.1, 1.1])
   .rangeRound([height, 0])
 
-  const l = area()
-  .x(d => scaleX(d[0]))
-  .y0(height)
-  .y1(d => scaleY(d[1]))
+  const line = d3Area<Point>()
+  .x((_, i) => x(i))
+  .y(d => y(d.acpl))
+
+  const area = d3Area<Point>()
+  .x((_, i) => x(i))
+  .y1(d => d.acpl <= 0 ? y(0) : y(d.acpl))
+
+  g.datum(graphData)
+
+  g.append('line')
+  .attr('class', 'zeroline')
+  .attr('x1', 0)
+  .attr('x2', width)
+  .attr('y1', y(0))
+  .attr('y2', y(0))
+
+  g.append('clipPath')
+  .attr('id', 'clip-below')
+  .append('path')
+  .attr('d', area.y0(d => y(d.acpl)))
+
+  g.append('clipPath')
+  .attr('id', 'clip-above')
+  .append('path')
+  .attr('d', area.y0(y(0)))
 
   g.append('path')
-  .attr('class', 'path')
-  .attr('fill', 'steelblue')
-  .attr('stroke', 'steelblue')
-  .attr('stroke-linejoin', 'round')
-  .attr('stroke-linecap', 'round')
-  .attr('stroke-width', 0)
-  .attr('d', l(data) as string)
+  .attr('class', 'area above')
+  .attr('clip-path', 'url(#clip-above)')
+  .attr('d', area)
+
+  g.append('path')
+  .attr('class', 'area below')
+  .attr('clip-path', 'url(#clip-below)')
+  .attr('d', area.y0(d => d.acpl <= 0 ? y(d.acpl) : y(0)))
+
+  g.append('path')
+  .attr('class', 'line')
+  .attr('d', line)
 }
 
-
-
-export function makeSerieData(d: AnalyseData): Point[] {
+function makeSerieData(d: AnalyseData): Point[] {
   return d.treeParts.slice(1).map((node) => {
     const ply = node.ply!
     const color = ply & 1

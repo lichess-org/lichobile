6c1e3ea5c964e8d812b2d96b8561d9244c6bbf6e
Vincent Velociter <vincent.velociter@gmail.com>
2018-03-30 10:34:16 +0200

    Improve study interface

--- src/lichess/interfaces/study.ts
+++ src/lichess/interfaces/study.ts
@@ -2,97 +2,99 @@ import { AnalyseData } from './analyse'
 import { Tree } from '../../ui/shared/tree'
 
 export interface StudyData {
-  id: string
-  name: string
-  members: StudyMemberMap
-  position: Position
-  ownerId: string
-  settings: StudySettings
-  visibility: 'public' | 'private'
-  createdAt: number
-  from: string
-  likes: number
-  isNew?: boolean
-  liked: boolean
-  features: StudyFeatures
-  chapters: StudyChapterMeta[]
-  chapter: StudyChapter
-  secondsSinceUpdate: number
+  readonly id: string
+  readonly name: string
+  readonly members: StudyMemberMap
+  readonly position: Position
+  readonly ownerId: string
+  readonly settings: StudySettings
+  readonly visibility: 'public' | 'private' | 'unlisted'
+  readonly createdAt: number
+  readonly from: string
+  readonly likes: number
+  readonly isNew?: boolean
+  readonly liked: boolean
+  readonly features: StudyFeatures
+  readonly chapters: ReadonlyArray<StudyChapterMeta>
+  readonly chapter: StudyChapter
+  readonly secondsSinceUpdate: number
 }
 
+type UserSelection = 'nobody' | 'owner' | 'contributor' | 'member' | 'everyone'
+
 export interface StudySettings {
-  computer: string
-  explorer: string
-  cloneable: string
-  chat: string
-  sticky: boolean
+  readonly computer: string
+  readonly explorer: string
+  readonly cloneable: string
+  readonly chat: UserSelection
+  readonly sticky: boolean
 }
 
 export interface ReloadData {
-  analysis: AnalyseData
-  study: StudyData
+  readonly analysis: AnalyseData
+  readonly study: StudyData
 }
 
 interface Position {
-  chapterId: string
-  path: Tree.Path
+  readonly chapterId: string
+  readonly path: Tree.Path
 }
 
 export interface StudyFeatures {
-  cloneable: boolean
-  chat: boolean
-  sticky: boolean
+  readonly cloneable: boolean
+  readonly chat: boolean
+  readonly sticky: boolean
 }
 
 export interface StudyChapterMeta {
-  id: string
-  name: string
+  readonly id: string
+  readonly name: string
 }
 
 export interface StudyChapter {
-  id: string
-  name: string
-  ownerId: string
-  setup: StudyChapterSetup
-  tags: TagArray[]
-  practice: boolean
-  conceal?: number
-  gamebook: boolean
-  features: StudyChapterFeatures
-  description?: string
-  relay?: StudyChapterRelay
+  readonly id: string
+  readonly name: string
+  readonly ownerId: string
+  readonly setup: StudyChapterSetup
+  readonly tags: ReadonlyArray<TagTuple>
+  readonly practice: boolean
+  readonly conceal?: number
+  readonly gamebook: boolean
+  readonly features: StudyChapterFeatures
+  readonly description?: string
+  readonly relay?: StudyChapterRelay
 }
 
 export interface StudyChapterRelay {
-  path: Tree.Path
-  secondsSinceLastMove?: number
-  lastMoveAt?: number
+  readonly path: Tree.Path
+  readonly secondsSinceLastMove?: number
+  readonly lastMoveAt?: number
 }
 
 interface StudyChapterSetup {
-  gameId?: string
-  variant: {
-    key: string
-    name: string
+  readonly gameId?: string
+  readonly variant: {
+    readonly key: string
+    readonly name: string
   }
-  orientation: Color
-  fromFen?: string
+  readonly orientation: Color
+  readonly fromFen?: string
 }
 
 interface StudyChapterFeatures {
-  computer: boolean
-  explorer: boolean
+  readonly computer: boolean
+  readonly explorer: boolean
 }
 
 export type StudyMember = any
 
 export interface StudyMemberMap {
-  [id: string]: StudyMember
+  readonly [id: string]: StudyMember | undefined
 }
 
-export type TagTypes = string[]
-export type TagArray = [string, string]
+export type TagTypes = ReadonlyArray<string>
+export type TagTuple = [string, string]
 
 export interface LocalPaths {
-  [chapterId: string]: Tree.Path
+  readonly [chapterId: string]: Tree.Path | undefined
 }

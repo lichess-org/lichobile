9d0b35a5d5fda850c00bd8cf416c380433c776b3
Vincent Velociter <vincent.velociter@gmail.com>
2018-04-12 09:17:08 +0200

    Tweak about page

--- src/ui/helper/index.ts
+++ src/ui/helper/index.ts
@@ -365,15 +365,15 @@ export function findElByClassName(e: Event, className: string) {
     target : findParentBySelector(target, '.' + className)
 }
 
-export function externalLink(text: string, url: string): Mithril.Children {
-  return h('span', { class: 'link' }, [
-    h('a', { href: '#', onClick: 'window.open("' + url + '")' }, text),
-    h('span', { class: 'fa fa-external-link' })
-    ])
+export function externalLink(text: string, url: string): Mithril.Child {
+  return h('a', {
+    className: 'external_link',
+    onClick: `window.open('${url}')`,
+  }, text)
 }
 
-export function internalLink(text: string, route: string): Mithril.Children {
-  return h('span', { class: 'link' }, [
-    h('span', { oncreate: ontap(() => { router.set(route) }) }, text),
-  ])
+export function internalLink(text: string, route: string): Mithril.Child {
+  return h('a', {
+    oncreate: ontap(() => { router.set(route) })
+  }, text)
 }

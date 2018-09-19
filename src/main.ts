e2274bb85c4b9062856603e73318f83394754f71
Vincent Velociter <vincent.velociter@gmail.com>
2018-09-19 11:25:23 +0200

    Remove raven

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,7 @@ import 'whatwg-fetch'
 
 import './moment'
 
-import * as Raven from 'raven-js'
 import * as debounce from 'lodash/debounce'
-import globalConfig from './config'
 import { hasNetwork } from './utils'
 import { syncWithNowPlayingGames } from './utils/offlineGames'
 import redraw from './utils/redraw'
@@ -72,12 +70,6 @@ function main() {
   // TODO we may want to remove this and call only on purpose
   window.cordova.plugins.Keyboard.disableScroll(true)
   window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false)
-
-  if (globalConfig.mode === 'release' && globalConfig.sentryDSN) {
-    Raven.config(globalConfig.sentryDSN, {
-      release: window.AppVersion ? window.AppVersion.version : 'snapshot-dev'
-    }).install()
-  }
 
   if (cordova.platformId === 'android') {
       window.StatusBar.backgroundColorByHexString('#111')
@@ -170,8 +162,8 @@ function getPools() {
 
 document.addEventListener('deviceready',
   // i18n must be loaded before any rendering happens
-  () => Raven.context(() => {
+  () => {
     loadPreferredLanguage().then(main)
-  }),
+  },
   false
 )

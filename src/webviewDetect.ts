b51007c5b1db8717cf1f92610661882996b855a0
Vincent Velociter <vincent.velociter@gmail.com>
2018-09-23 13:21:22 +0200

    Refactor to have reusable prompt

--- src/webviewDetect.ts
+++ src/webviewDetect.ts
@@ -1,6 +1,7 @@
+import * as h from 'mithril/hyperscript'
 import { safeStringToNum } from './utils'
-import ButtonHandler from './ui/helper/button'
-import { getButton, getAnchor } from './ui/helper'
+import promptDialog from './prompt'
+import { getAnchor, ontap } from './ui/helper'
 
 const osPattern = /Android ([.0-9]+)/
 const webviewPattern = /Chrome\/([.0-9]+)/
@@ -20,23 +21,13 @@ export default function detectWebview() {
   }
 
   function showPrompt() {
-    const wrapper = document.createElement('div')
-    wrapper.className = 'overlay_popup_wrapper fade-in'
-    wrapper.id = 'webviewPrompt'
-    const box = document.createElement('div')
-    box.className = 'overlay_popup native_scroller'
-    box.innerHTML =
-    '<header><strong>Warning</strong></header>' +
-    '<div class="popup_content">' +
-      (osVersion >= 7 ? chromeUpgradeMsg : webviewUpgradeMsg) +
-      '<div class="okBtn">' +
-        '<button>OK</button>'
-      '</div>' +
-    '</div>'
-    ButtonHandler(box, () => removePrompt(), undefined, undefined, undefined, undefined, getButton)
-    ButtonHandler(box, openStore, undefined, undefined, undefined, undefined, getAnchor)
-    wrapper.appendChild(box)
-    document.body.appendChild(wrapper)
+    const content = h('div', {
+      oncreate: ontap(openStore, undefined, undefined, getAnchor)
+    }, upgradeMsg().concat([
+        h('div.buttons', h('button', { oncreate: ontap(promptDialog.hide) }, 'OK')),
+      ])
+    )
+    promptDialog.show(content, 'Warning')
   }
 
   function openStore(e: TouchEvent) {
@@ -46,20 +37,19 @@ export default function detectWebview() {
     }
   }
 
-  function removePrompt() {
-    const el = document.getElementById('webviewPrompt')
-    if (el) {
-      document.body.removeChild(el)
-    }
+  function upgradeMsg() {
+    const appName = osVersion >= 7 ? 'Chrome' : 'System WebView'
+    const appPackage = osVersion >= 7 ? 'com.android.chrome' : 'com.google.android.webview'
+    return [
+      h('p', `Lichess relies heavily on ${appName} application.`),
+      h('p', `We detected the ${appName} version you have currently installed (${webviewVersion}) is too old: lichess might not work properly.`),
+      h('p', [
+        'To fix this, please ',
+        h('a', {
+          href: '#',
+          'data-id': appPackage,
+        }, `upgrade ${appName} application.`)
+      ])
+    ]
   }
-
-  const chromeUpgradeMsg =
-    '<p>Lichess relies heavily on Chrome application.</p>' +
-    `<p>We detected the Chrome version you are using (${webviewVersion}) is too old: lichess might not work properly.</p>` +
-    `<p>To fix this, please <a href="#" data-id="com.android.chrome">upgrade Chrome application</a>.</p>`
-
-  const webviewUpgradeMsg =
-    '<p>Lichess relies heavily on System WebView application.</p>' +
-    `<p>We detected the System WebView version you are using (${webviewVersion}) is too old: lichess will not work properly.</p>` +
-    `<p>To fix this, please <a href="#" data-id="com.google.android.webview">upgrade System WebView application</a>.</p>`
 }

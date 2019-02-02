4e924d9f503f4c5b3450e36a3682ee907d69b032
Lakin Wecker <lakin@structuredabstraction.com>
2019-02-02 13:45:41 -0700

    Adding .native_scroller to the #free_content div, which fixes scrolling.

--- src/ui/layout.ts
+++ src/ui/layout.ts
@@ -55,7 +55,7 @@ export default {
     return h('div.view-container', { className: bgClass(background) }, [
       h('main#page', { oncreate: handleMenuOpen }, [
         h('header.main_header', header),
-        h('div#free_content.content', content),
+        h('div#free_content.content.native_scroller', content),
         footer ? h('footer.main_footer', footer) : null,
         h('div#menu-close-overlay.menu-backdrop', { oncreate: menu.backdropCloseHandler })
       ]),

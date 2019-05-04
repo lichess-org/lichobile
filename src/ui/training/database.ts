ced4eddeb73bc9d826488d17136b4be51a57f995
Vincent Velociter <vincent.velociter@gmail.com>
2019-05-04 15:15:48 +0200

    Remove compat layer

--- src/ui/training/database.ts
+++ src/ui/training/database.ts
@@ -21,16 +21,6 @@ type UserId = string
 
 function fetch(userId: UserId): Promise<UserOfflineData | null> {
   return asyncStorage.getItem<UserOfflineData>(`offlinePuzzles.${userId}`)
-  .then(data => {
-    if (!data) {
-      // compat layer for old storage key
-      // TODO remove in 2 versions (from 6.0.0)
-      return asyncStorage.getItem<{ [key: string]: UserOfflineData }>('training.offlinePuzzles')
-      .then(data => data && data[userId] || null)
-    } else {
-      return data
-    }
-  })
 }
 
 function save(userId: UserId, userData: UserOfflineData): Promise<UserOfflineData> {

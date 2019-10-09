49850abb23ed76f25d1d4cc77efb0308a051cb5a
Vincent Velociter <vincent.velociter@gmail.com>
2019-10-09 17:57:20 +0200

    More wip on build

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,9 @@
 const path = require('path');
-const { exec } = require('child_process');
-const source = require('vinyl-source-stream');
 const minimist = require('minimist');
 const gulp = require('gulp');
-const rollup = require('rollup')
-const resolve = require('rollup-plugin-node-resolve')
-const commonjs = require('rollup-plugin-commonjs')
-const json = require('rollup-plugin-json')
-const terser = require('rollup-plugin-terser')
 const chalk = require('chalk');
 const log = require('fancy-log');
 const preprocess = require('gulp-preprocess');
-const stylus = require('gulp-stylus');
-const autoprefixer = require('gulp-autoprefixer');
-const rename = require('gulp-rename');
-const buffer = require('vinyl-buffer')
 
 const SRC = 'src'
 const DEST = 'www'
@@ -30,19 +19,12 @@ const minimistOptions = {
 };
 const options = minimist(process.argv.slice(2), minimistOptions);
 
-const paths = {
-  styles: ['src/styl/reset.styl', 'src/styl/common.styl', 'src/styl/form.styl',
-    'src/styl/overlay.styl', 'src/styl/overlay-popup.styl', 'src/styl/*.styl',
-    'src/**/*.styl'
-  ]
-};
-
 function logErrorAndExit(error) {
   log.error(chalk.red(error.message))
   process.exit(1)
 }
 
-gulp.task('html', () => {
+function html() {
   const context = require('./' + options.env);
   context.TARGET = options.target;
   context.MODE = options.mode;
@@ -52,61 +34,11 @@ gulp.task('html', () => {
     .pipe(preprocess({context: context}))
     .on('error', logErrorAndExit)
     .pipe(gulp.dest(DEST));
-});
+}
 
-gulp.task('styl', () => {
-  return gulp.src(SRC + '/styl/index.styl')
-  .pipe(stylus({
-    compress: options.mode === 'release'
-  }))
-  .pipe(autoprefixer())
-  .pipe(rename('app.css'))
-  .on('error', logErrorAndExit)
-  .pipe(gulp.dest(DEST + '/css/compiled/'));
-});
+function launchWatch() {
+  gulp.watch(['src/index.html'], html);
+}
 
-gulp.task('compile', cb => {
-  exec('npx tsc --outDir build/', (err, stdout, stderr) => {
-    log.error(chalk.red(stdout))
-    cb(err)
-  })
-})
-
-gulp.task('bundle', (cb) => {
-  // return rollup.rollup({
-  //   input: 'build/main.js',
-  //   plugins: [
-  //     resolve(),
-  //     commonjs(),
-  //     json(),
-  //     options.mode === 'release' && terser(),
-  //   ],
-  //   onwarn(warning, warn) {
-  //     if (warning.code === 'CIRCULAR_DEPENDENCY') return
-  //     if ( warning.code === 'THIS_IS_UNDEFINED' ) return
-  //     warn(warning)
-  //   }
-  // }).then(bundle => {
-  //   return bundle.write({
-  //     file: 'www/app.js',
-  //     format: 'iife',
-  //     sourcemap: true
-  //   });
-  // });
-  exec('npx rollup -c', (err, stdout, stderr) => {
-    log.info(stderr)
-    cb(err)
-  })
-});
-
-gulp.task('watch-scripts', () => {
-});
-
-// Watch Files For Changes
-gulp.task('launch-watch', () => {
-  gulp.watch(paths.styles, gulp.parallel(['styl']));
-  gulp.watch(['src/index.html', 'env.json'], gulp.parallel(['html']));
-});
-
-gulp.task('default', gulp.parallel(['html', 'styl', 'bundle']));
-gulp.task('watch', gulp.series(['html', 'styl', 'watch-scripts', 'launch-watch']));
+exports.html = html
+exports.watch = gulp.series(html, launchWatch)

8a4122d5928fd30c39e762a36237bc5be27d099c
Vincent Velociter <vincent.velociter@gmail.com>
2019-10-10 11:46:43 +0200

    Fix game view

--- src/ui/shared/round/view/roundView.tsx
+++ src/ui/shared/round/view/roundView.tsx
@@ -48,7 +48,7 @@ export default function view(ctrl: OnlineRound) {
 
 export function renderMaterial(material: Material) {
   const tomb = Object.keys(material.pieces).map((role: Role) =>
-    h('div.tomb', { key: role }, range(material.pieces[role])
+    h('div.tomb', range(material.pieces[role])
       .map(_ => h('piece', { className: role }))
     )
   )

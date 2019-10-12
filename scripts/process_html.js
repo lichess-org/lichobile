fe97e44c525fb9b108a36c6155bc84c2ff3795e8
Vincent Velociter <vincent.velociter@gmail.com>
2019-10-12 11:02:20 +0200

    Remove onesignal and simplify build

--- scripts/process_html.js
+++ scripts/process_html.js
@@ -6,16 +6,12 @@ const Mustache = require('mustache')
 
 const rootDir = path.join(__dirname, '..')
 
-// web | native
-const target = process.env.APP_TARGET || 'web'
 // dev | prod
 const mode = process.env.APP_MODE || 'dev'
 // dev | prod | custom
 const confKey = process.env.APP_CONFIG || 'dev'
 
 const config = require(path.join(rootDir, 'appconfig.' + confKey + '.json'))
-config.target = target
-config.webTarget = target === 'web'
 config.appMode = mode
 
 console.log('Will inject config:')

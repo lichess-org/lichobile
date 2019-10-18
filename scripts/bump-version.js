033a03724a0d8b2dd8cdf941eba33d366e6dcbc0
Vincent Velociter <vincent.velociter@gmail.com>
2019-10-18 17:59:11 +0200

    Fix bump version script

---
+++ scripts/bump-version.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+
+const rootDir = path.join(__dirname, '..')
+
+const androidManifestPath = path.join(rootDir, 'android', 'app', 'src', 'main', 'AndroidManifest.xml')
+const androidManifestText = fs.readFileSync(androidManifestPath).toString()
+
+const infoPlistPath = path.join(rootDir, 'ios', 'App', 'App', 'Info.plist')
+const infoPlistText = fs.readFileSync(infoPlistPath).toString()
+
+// when this script is executed (by npm-version) the new version is accessible
+// in package.json
+const newVersion = require('../package.json').version;
+const newVersionParts = newVersion.split('.')
+const newVersionCode =
+  newVersionParts[0].padEnd(2, 0) +
+  newVersionParts.slice(1).map(p => p.padStart(2, 0)).join('') +
+  '9'
+
+// android
+let newAndroidManifestText = androidManifestText.replace(
+  /(android:versionName=")([^"]+)(")/,
+  '$1' + newVersion + '$3'
+)
+newAndroidManifestText = newAndroidManifestText.replace(
+  /(android:versionCode=")([^"]+)(")/,
+  '$1' + newVersionCode + '$3'
+)
+fs.writeFileSync(androidManifestPath, newAndroidManifestText)
+
+// ios
+let newPlistText = infoPlistText.replace(
+  /(CFBundleShortVersionString.+?[\s\S]+?<string>)([^<]+)/,
+  '$1' + newVersion
+)
+fs.writeFileSync(infoPlistPath, newPlistText)

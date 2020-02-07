ac6127507bb792eccb05181f3bde6bf5473c54ad
Vincent Velociter <vincent.velociter@gmail.com>
2020-02-07 18:42:57 +0100

    Fix game not loading when vibrate enabled on iOS

--- src/vibrate.ts
+++ src/vibrate.ts
@@ -1,10 +1,14 @@
+import { Plugins } from '@capacitor/core'
 import settings from './settings'
 
 let shouldVibrate: boolean = settings.general.vibrateOnGameEvents()
 
 export default {
   quick() {
-    if (shouldVibrate) window.navigator.vibrate(150)
+    if (shouldVibrate) {
+      if (window.navigator.vibrate) window.navigator.vibrate(150)
+      else Plugins.Haptics.vibrate()
+    }
   },
   onSettingChange(v: boolean) {
     shouldVibrate = v

ae115516af086b483e5a9de7e15103f1e58e53dc
Guillermo Manjon <manjon.mateo@gmail.com>
2020-06-22 10:41:17 +0200

    Fix square coordinates settings styles Add slide up/down menu behaviour Use user defined board to show opacity settings

--- src/ui/settings/gameDisplay.ts
+++ src/ui/settings/gameDisplay.ts
@@ -1,34 +1,50 @@
 import * as helper from '../helper'
-import { dropShadowHeader, backButton } from '../shared/common'
+import {backButton, dropShadowHeader} from '../shared/common'
 import formWidgets from '../shared/form'
-import { onPieceNotationChange } from '../shared/round/view/replay'
+import {onPieceNotationChange} from '../shared/round/view/replay'
 import layout from '../layout'
 import i18n from '../../i18n'
 import settings from '../../settings'
 import h from 'mithril/hyperscript'
+import redraw from '../../utils/redraw'
+
+function toggleSquareCoordSettings(show: boolean) {
+    const el = document.getElementById('square-coords-settings-list')
+    if (el) {
+        el.className = show ? 'open' : 'closed'
+    }
+}
 
 function renderBody() {
   return [
     h('ul.native_scroller.page.settings_list.game', [
       h('li.list_item', formWidgets.renderCheckbox(i18n('boardCoordinates'), 'coords', settings.game.coords)),
-      h('li.list_item', formWidgets.renderCheckbox(i18n('squareCoordinates'), 'squareCoords', settings.game.squareCoords.enabled, function() {})),
-      h('ul.submenu', [
-          h('li.list_item.square-coords-opacity-setting', [
-              formWidgets.renderSlider(
-                  'white opacity', 'whiteOpacity', 0, 100, 1, settings.game.squareCoords.whiteSquaresOpacity,
-                  function() {}
-              )]),
-          h('li.list_item.square-coords-opacity-setting', [
-              formWidgets.renderSlider(
-                  'black opacity', 'blackOpacity', 0, 100, 1, settings.game.squareCoords.blackSquaresOpacity,
-                  function() {}
-              )]),
-          h('li.list_item.square-coords-opacity-setting', [
-              h('div',  {className: 'overlay-coordinates-sample-wrapper'}, [
-                  h('div', {className: 'overlay-coordinates-sample board'}),
-                  h('div', {className: 'overlay-coordinates-sample white-squares', opacity : (settings.game.squareCoords.whiteSquaresOpacity() / 100).toString(10)}),
-                  h('div', {className: 'overlay-coordinates-sample black-squares', opacity : (settings.game.squareCoords.blackSquaresOpacity() / 100).toString(10)})
-                  ]
+      h('li.list_item', [
+          formWidgets.renderCheckbox(i18n('squareCoordinates'), 'squareCoords', settings.game.squareCoords.enabled, toggleSquareCoordSettings),
+          h('ul#square-coords-settings-list', {class: settings.game.squareCoords.enabled() ? 'open' : 'closed'}, [
+              h('li.list_item.square-coords-opacity-setting', [
+                  formWidgets.renderSlider(
+                      'white opacity', 'whiteOpacity', 0, 100, 1, settings.game.squareCoords.whiteSquaresOpacity,
+                      redraw
+                  )]),
+              h('li.list_item.square-coords-opacity-setting', [
+                  formWidgets.renderSlider(
+                      'black opacity', 'blackOpacity', 0, 100, 1, settings.game.squareCoords.blackSquaresOpacity,
+                      redraw
+                  )]),
+              h('li.list_item.square-coords-opacity-setting', [
+                  h('div', {className: 'overlay-coordinates-sample-wrapper'}, [
+                          h('div', {className: 'overlay-coordinates-sample board'}),
+                          h('div', {
+                              className: 'overlay-coordinates-sample white-squares',
+                              style: {opacity: (settings.game.squareCoords.whiteSquaresOpacity() / 100).toString(10)}
+                          }),
+                          h('div', {
+                              className: 'overlay-coordinates-sample black-squares',
+                              style: {opacity: (settings.game.squareCoords.blackSquaresOpacity() / 100).toString(10)}
+                          })
+                      ]
+                  )]
               )]
           )
       ]),
@@ -72,7 +88,6 @@ function renderBody() {
 
 export default {
   oncreate: helper.viewSlideIn,
-  // controller() {},
   view() {
     const header = dropShadowHeader(null, backButton(i18n('gameDisplay')))
     return layout.free(header, renderBody())

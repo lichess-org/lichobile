2e650807a10296661bd5f2901986a86095740b98
Joe Stein <joe@splitwise.com>
2020-12-19 16:51:13 -0800

    Add types to AnalyseData forecast

---
+++ src/lichess/interfaces/forecast.ts
@@ -0,0 +1,12 @@
+export interface ForecastData {
+  onMyTurn?: boolean;
+  steps?: ForecastStep[][];
+}
+
+export interface ForecastStep {
+  ply: Ply;
+  uci: Uci;
+  san: San;
+  fen: Fen;
+  check?: Key;
+}

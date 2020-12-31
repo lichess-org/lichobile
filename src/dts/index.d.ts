4633cb195b4ad87b96185d42ac51dfa25a6e178f
Joe Stein <joe@splitwise.com>
2020-12-31 08:32:17 -0800

    Remove setTimeout type declaration

--- src/dts/index.d.ts
+++ src/dts/index.d.ts
@@ -6,6 +6,3 @@
 /// <reference path="signals.d.ts" />
 /// <reference path="misc.d.ts" />
 /// <reference path="mithril.d.ts" />
-
-// Avoid conflicts between NodeJS's setTimeout and window.setTimeout
-declare function setTimeout(handler: TimerHandler, timeout?: number, ...arguments: any[]): number;

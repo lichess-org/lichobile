4cc17f6b951aceced00f2c32b32a5d60a253e18d
Vincent Velociter <vincent.velociter@gmail.com>
2021-01-09 13:52:26 +0100

    Round memory to next multiple of 16

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,7 @@ export default function appInit(
     uuid: info.uuid,
     appVersion: info.appVersion,
     cpuCores,
-    stockfishMaxMemory: sfMaxMem,
+    stockfishMaxMemory: Math.ceil(sfMaxMem / 16.0) * 16,
   }
 
   if (Capacitor.platform === 'ios') {

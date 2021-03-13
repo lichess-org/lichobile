9c1b89cd5719d10c73f2ed9f878dddd9d254371d
Joe Stein <joeaarons@gmail.com>
2021-03-13 11:16:09 -0500

    Apply eslint to script/

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,39 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
+  'env': {
+    'browser': true,
+    'es2021': true
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
+  'overrides': [
+    {
+      'files': ['*.ts', '*.tsx'],
+      'plugins': ['@typescript-eslint'],
+      'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+      ],
+      'rules': {
+        '@typescript-eslint/triple-slash-reference': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/semi': ['error', 'never']
+      }
+    },
+    {
+      'files': [
+        '.eslintrc.js',
+        './scripts/**/*.js',
+      ],
+      'env': {
+        'node': true,
+      },
+    },
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 12,
-    "sourceType": "module"
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module'
   },
-  "plugins": ["@typescript-eslint"],
-  "rules": {
-    "semi": "off",
-    "quotes": ["error", "single", { "avoidEscape": true }],
-    "@typescript-eslint/triple-slash-reference": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/semi": ["error", "never"]
+  'rules': {
+    'semi': 'off',
+    'quotes': ['error', 'single', { 'avoidEscape': true }],
   }
 }

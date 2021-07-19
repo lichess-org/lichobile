ebeeaaf2614930276b447f734763f8f80ad3e37c
Joe Stein <joeaarons@gmail.com>
2021-07-19 12:03:09 -0400

    Declare types for CPUInfo plugin

--- src/dts/plugins.d.ts
+++ src/dts/plugins.d.ts
@@ -1,12 +1,17 @@
-import { SoundEffectPlugin } from 'capacitor-sound-effect';
+import { SoundEffectPlugin } from 'capacitor-sound-effect'
 
 declare module '@capacitor/core' {
   interface PluginRegistry {
     Badge: BadgePlugin
+    CPUInfo: CPUInfoPlugin
     SoundEffect: SoundEffectPlugin
   }
 }
 
 interface BadgePlugin {
   setNumber: (opts: {badge: number}) => Promise<void>
+}
+
+interface CPUInfoPlugin {
+  nbCores: () => Promise<{value: number}>
 }

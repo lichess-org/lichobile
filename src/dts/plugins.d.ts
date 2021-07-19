c80d0f0a46fb3483aded1b5a64d5a2130751e8d5
Joe Stein <joeaarons@gmail.com>
2021-07-19 12:00:02 -0400

    Declare types for Badge plugin

--- src/dts/plugins.d.ts
+++ src/dts/plugins.d.ts
@@ -2,6 +2,11 @@ import { SoundEffectPlugin } from 'capacitor-sound-effect';
 
 declare module '@capacitor/core' {
   interface PluginRegistry {
+    Badge: BadgePlugin
     SoundEffect: SoundEffectPlugin
   }
+}
+
+interface BadgePlugin {
+  setNumber: (opts: {badge: number}) => Promise<void>
 }

b5d3d54c4906b7ac9aeaa174599d626cffcf2917
Steven Pav <shabbychef@gmail.com>
2021-08-30 21:58:05 -0700

    WIP: pass on promotion piece from stockfish
    does not seem to be respected by addMove, however.

--- src/ui/ai/AiRound.ts
+++ src/ui/ai/AiRound.ts
@@ -225,9 +225,10 @@ export default class AiRound implements AiRoundInterface, PromotingInterface {
   public onEngineMove = (bestmove: string) => {
     const from = <Key>bestmove.slice(0, 2)
     const to = <Key>bestmove.slice(2, 4)
+    const role = <Role>bestmove.slice(4, 5)
     this.vm.engineSearching = false
     this.chessground.apiMove(from, to)
-    this.replay.addMove(from, to)
+    this.replay.addMove(from, to, role)
     redraw()
   }
 

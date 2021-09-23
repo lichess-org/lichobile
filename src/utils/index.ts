5a067b98c4ebd4727c5c06b542a9fe125409655c
Joe Stein <569991+jas14@users.noreply.github.com>
2021-09-23 11:26:46 -0500

    Update tournament time options

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -360,3 +360,15 @@ export function animationDuration(pref: boolean): number {
 export function randomColor(): Color {
   return Math.random() > 0.5 ? 'white' : 'black'
 }
+
+export function increments(min: number, max: number, step = 1): string[] {
+  const array = []
+  for (let i = min; i <= max; i += step) {
+    array.push(i.toString())
+  }
+  return array
+}
+
+export function incrementTuples(min: number, max: number, step = 1): [string, string][] {
+  return increments(min, max, step).map(tupleOf)
+}

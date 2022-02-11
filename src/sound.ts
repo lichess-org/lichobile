ec5ea0c71f811d14c8333da9caf76d0d56cff10c
Vincent Velociter <vincent.velociter@gmail.com>
2022-02-11 11:14:48 +0100

    Don't play sounds while in background
    Closes #1997

--- src/sound.ts
+++ src/sound.ts
@@ -1,13 +1,15 @@
+import throttle from 'lodash-es/throttle'
 import { SoundEffect } from 'capacitor-sound-effect'
 import { DeviceInfo } from '@capacitor/device'
 import settings from './settings'
-import throttle from 'lodash-es/throttle'
+import { isForeground } from './utils/appMode'
 
-let shouldPlay: boolean
+function shouldPlay(): boolean {
+  return settings.general.sound() && isForeground()
+}
 
 export default {
   load(info: DeviceInfo): Promise<void> {
-    shouldPlay = settings.general.sound()
     const ext = info.platform === 'ios' ? '.aifc' : '.mp3'
 
     return Promise.all([
@@ -22,39 +24,36 @@ export default {
     ]).then(() => { /* noop */ })
   },
   move() {
-    if (shouldPlay) SoundEffect.play({ id: 'move' })
+    if (shouldPlay()) SoundEffect.play({ id: 'move' })
   },
   throttledMove: throttle(() => {
-    if (shouldPlay) SoundEffect.play({ id: 'move' })
+    if (shouldPlay()) SoundEffect.play({ id: 'move' })
   }, 50),
   capture() {
-    if (shouldPlay) SoundEffect.play({ id: 'capture' })
+    if (shouldPlay()) SoundEffect.play({ id: 'capture' })
   },
   throttledCapture: throttle(() => {
-    if (shouldPlay) SoundEffect.play({ id: 'capture' })
+    if (shouldPlay()) SoundEffect.play({ id: 'capture' })
   }, 50),
   explosion() {
-    if (shouldPlay) SoundEffect.play({ id: 'explosion' })
+    if (shouldPlay()) SoundEffect.play({ id: 'explosion' })
   },
   throttledExplosion: throttle(() => {
-    if (shouldPlay) SoundEffect.play({ id: 'explosion' })
+    if (shouldPlay()) SoundEffect.play({ id: 'explosion' })
   }, 50),
   lowtime() {
-    if (shouldPlay) SoundEffect.play({ id: 'lowtime' })
+    if (shouldPlay()) SoundEffect.play({ id: 'lowtime' })
   },
   dong() {
-    if (shouldPlay) SoundEffect.play({ id: 'dong' })
+    if (shouldPlay()) SoundEffect.play({ id: 'dong' })
   },
   berserk() {
-    if (shouldPlay) SoundEffect.play({ id: 'berserk' })
+    if (shouldPlay()) SoundEffect.play({ id: 'berserk' })
   },
   clock() {
-    if (shouldPlay) SoundEffect.play({ id: 'clock' })
+    if (shouldPlay()) SoundEffect.play({ id: 'clock' })
   },
   confirmation() {
-    if (shouldPlay) SoundEffect.play({ id: 'confirmation' })
+    if (shouldPlay()) SoundEffect.play({ id: 'confirmation' })
   },
-  onSettingChange(v: boolean) {
-    shouldPlay = v
-  }
 }

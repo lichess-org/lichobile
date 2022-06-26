61b4f31dc3e986780db606e5129eb68f748496ac
Vincent Velociter <vincent.velociter@gmail.com>
2022-06-26 11:27:42 +0200

    Use haptic when possible for ai games

--- src/ui/ai/AiRound.ts
+++ src/ui/ai/AiRound.ts
@@ -286,7 +286,7 @@ export default class AiRound implements AiRoundInterface, PromotingInterface {
     } else {
       sound.move()
     }
-    vibrate.quick()
+    vibrate.tap()
   }
 
   private onUserNewPiece = (role: Role, key: Key) => {

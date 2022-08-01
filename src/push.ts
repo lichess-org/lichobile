646bfc1bdd0c3f3120b14be8ac24ce6061aac1a5
Jonathan Gamble <101470903+schlawg@users.noreply.github.com>
2022-08-01 15:42:35 -0500

    add streamStart and forumMention notification handlers
    launching in an unauthenticated browser session.

--- src/push.ts
+++ src/push.ts
@@ -1,96 +1,94 @@
-import {
-  PushNotifications,
-  PushNotificationSchema,
-  Token,
-  ActionPerformed
-} from '@capacitor/push-notifications'
-import { fetchText } from './http'
-import challengesApi from './lichess/challenges'
-import router from './router'
-import session from './session'
-import settings from './settings'
-import { handleXhrError } from './utils'
-import { isForeground } from './utils/appMode'
+import { PushNotifications, PushNotificationSchema, Token, ActionPerformed } from '@capacitor/push-notifications';
+import { fetchText } from './http';
+import challengesApi from './lichess/challenges';
+import { openExternalBrowser } from './utils/browse';
+import router from './router';
+import session from './session';
+import settings from './settings';
+import { handleXhrError } from './utils';
+import { isForeground } from './utils/appMode';
 
 export default {
   init() {
-    PushNotifications.addListener('registration',
-      ({ value }: Token) => {
-        console.debug('Push registration success, FCM token: ' + value)
+    PushNotifications.addListener('registration', ({ value }: Token) => {
+      console.debug('Push registration success, FCM token: ' + value);
 
-        fetchText(`/mobile/register/firebase/${value}`, {
-          method: 'POST'
-        })
-        .catch(handleXhrError)
-      }
-    )
+      fetchText(`/mobile/register/firebase/${value}`, {
+        method: 'POST',
+      }).catch(handleXhrError);
+    });
 
-    PushNotifications.addListener('registrationError',
-      (error: any) => {
-        console.error('Error on registration: ' + JSON.stringify(error))
-      }
-    )
+    PushNotifications.addListener('registrationError', (error: any) => {
+      console.error('Error on registration: ' + JSON.stringify(error));
+    });
 
-    PushNotifications.addListener('pushNotificationReceived',
-      (notification: PushNotificationSchema) => {
-        if (isForeground()) {
-          switch (notification.data['lichess.type']) {
-            case 'corresAlarm':
-            case 'gameTakebackOffer':
-            case 'gameDrawOffer':
-            case 'challengeAccept':
-            case 'gameMove':
-            case 'gameFinish':
-              session.refresh()
-              break
-          }
+    PushNotifications.addListener('pushNotificationReceived', (notification: PushNotificationSchema) => {
+      if (isForeground()) {
+        switch (notification.data['lichess.type']) {
+          case 'corresAlarm':
+          case 'gameTakebackOffer':
+          case 'gameDrawOffer':
+          case 'challengeAccept':
+          case 'gameMove':
+          case 'gameFinish':
+          case 'forumMention':
+          case 'streamStart':
+            session.refresh();
+            break;
         }
       }
-    )
+    });
 
-    PushNotifications.addListener('pushNotificationActionPerformed',
-      (action: ActionPerformed) => {
-        if (action.actionId === 'tap') {
-          switch (action.notification.data['lichess.type']) {
-            case 'challengeAccept':
-              challengesApi.refresh()
-              router.set(`/game/${action.notification.data['lichess.challengeId']}`)
-              break
-            case 'challengeCreate':
-              router.set(`/game/${action.notification.data['lichess.challengeId']}`)
-              break
-            case 'corresAlarm':
-            case 'gameMove':
-            case 'gameFinish':
-            case 'gameTakebackOffer':
-            case 'gameDrawOffer':
-              router.set(`/game/${action.notification.data['lichess.fullId']}`)
-              break
-            case 'newMessage':
-              router.set(`/inbox/${action.notification.data['lichess.threadId']}`)
-              break
-          }
+    PushNotifications.addListener('pushNotificationActionPerformed', (action: ActionPerformed) => {
+      if (action.actionId === 'tap') {
+        switch (action.notification.data['lichess.type']) {
+          case 'challengeAccept':
+            challengesApi.refresh();
+            router.set(`/game/${action.notification.data['lichess.challengeId']}`);
+            break;
+          case 'challengeCreate':
+            router.set(`/game/${action.notification.data['lichess.challengeId']}`);
+            break;
+          case 'corresAlarm':
+          case 'gameMove':
+          case 'gameFinish':
+          case 'gameTakebackOffer':
+          case 'gameDrawOffer':
+            router.set(`/game/${action.notification.data['lichess.fullId']}`);
+            break;
+          case 'newMessage':
+            router.set(`/inbox/${action.notification.data['lichess.threadId']}`);
+            break;
+          case 'forumMention':
+            openExternalBrowser(
+              `https://lichess.org/forum/redirect/post/${action.notification.data['lichess.postId']}`
+            );
+            break;
+          case 'streamStart':
+            openExternalBrowser(
+              `https://lichess.org/streamer/${action.notification.data['lichess.streamerId']}/redirect`
+            );
+            break;
         }
       }
-    )
+    });
   },
 
   async register(): Promise<void> {
     if (settings.general.notifications.enable()) {
       PushNotifications.requestPermissions().then(result => {
         if (result.receive === 'granted') {
-          return PushNotifications.register()
+          return PushNotifications.register();
         } else {
-          return Promise.reject('Permission to use push denied')
+          return Promise.reject('Permission to use push denied');
         }
-      })
+      });
     }
 
-    return Promise.resolve()
+    return Promise.resolve();
   },
 
   unregister(): Promise<string> {
-    return fetchText('/mobile/unregister', { method: 'POST' })
-  }
-}
-
+    return fetchText('/mobile/unregister', { method: 'POST' });
+  },
+};
